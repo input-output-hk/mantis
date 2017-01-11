edbbb37e1054d2f78110b2d2cdcfbde98f61ef7f
Adam Smolarek <adam.smolarek@iohk.io>
2017-01-11 14:30:16 +0100

    add more examples

--- src/main/scala/io/iohk/ethereum/network/p2p/Message.scala
+++ src/main/scala/io/iohk/ethereum/network/p2p/Message.scala
@@ -2,7 +2,7 @@ package io.iohk.ethereum.network.p2p
 
 import io.iohk.ethereum.network.p2p.messages.CommonMessages._
 import io.iohk.ethereum.network.p2p.messages.WireProtocol._
-import io.iohk.ethereum.network.p2p.messages.{PV62 => pv62}
+import io.iohk.ethereum.network.p2p.messages.{PV62 => pv62, PV61 => pv61, PV63 => pv63}
 import io.iohk.ethereum.rlp
 
 object Message {
@@ -14,17 +14,26 @@ object Message {
   val PV63: Version = 63
 
   def decode(`type`: Int, payload: Array[Byte], protocolVersion: Version): Message = (protocolVersion, `type`) match {
+    //wire protocol
     case (_, Hello.code) => rlp.decode(payload)(Hello.rlpEndDec)
     case (_, Disconnect.code) => rlp.decode(payload)(Disconnect.rlpEndDec)
     case (_, Ping.code) => rlp.decode(payload)(Ping.rlpEndDec)
     case (_, Pong.code) => rlp.decode(payload)(Pong.rlpEndDec)
+
+    //common
     case (_, Status.code) => rlp.decode(payload)(Status.rlpEndDec)
     case (_, Transactions.code) => rlp.decode(payload)(Transactions.rlpEndDec)
+
+    case (PV61, pv61.NewBlockHashes.code) => rlp.decode(payload)(pv61.NewBlockHashes.rlpEndDec)
+
     case (PV62 | PV63, pv62.NewBlockHashes.code) => rlp.decode(payload)(pv62.NewBlockHashes.rlpEndDec)
     case (PV62 | PV63, pv62.GetBlockHeaders.code) => rlp.decode(payload)(pv62.GetBlockHeaders.rlpEndDec)
     case (PV62 | PV63, pv62.BlockHeaders.code) => rlp.decode(payload)(pv62.BlockHeaders.rlpEndDec)
     case (PV62 | PV63, pv62.GetBlockBodies.code) => rlp.decode(payload)(pv62.GetBlockBodies.rlpEndDec)
     case (PV62 | PV63, pv62.BlockBodies.code) => rlp.decode(payload)(pv62.BlockBodies.rlpEndDec)
+
+    case (PV63, pv63.GetNodeData.code) => rlp.decode(payload)(pv63.GetNodeData.rlpEndDec)
+
     case _ => throw new RuntimeException(s"Unknown message type: ${`type`}")
   }
 }
--- src/main/scala/io/iohk/ethereum/network/p2p/messages/PV61.scala
+++ src/main/scala/io/iohk/ethereum/network/p2p/messages/PV61.scala
@@ -1,5 +1,30 @@
 package io.iohk.ethereum.network.p2p.messages
 
+import akka.util.ByteString
+import io.iohk.ethereum.network.p2p.Message
+import io.iohk.ethereum.rlp.RLPImplicits._
+import io.iohk.ethereum.rlp._
+
 object PV61 {
+
+  object NewBlockHashes {
+    implicit val rlpEndDec = new RLPEncoder[NewBlockHashes] with RLPDecoder[NewBlockHashes] {
+      override def encode(obj: NewBlockHashes): RLPEncodeable = {
+        import obj._
+        RLPList(hashes.map(e => RLPValue(e.toArray[Byte])): _*)
+      }
+
+      override def decode(rlp: RLPEncodeable): NewBlockHashes = rlp match {
+        case rlpList: RLPList => NewBlockHashes(rlpList.items.map(e => ByteString(e: Array[Byte])))
+        case _ => throw new RuntimeException("Cannot decode NewBlockHashes")
+      }
+    }
+
+    val code: Int = 0x10 + 0x01
+  }
+
+  case class NewBlockHashes(hashes: Seq[ByteString]) extends Message {
+    override def code: Int = NewBlockHashes.code
+  }
 
 }
