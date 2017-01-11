ee34e28f65878da3be66f763e2a310a86ca6003e
ntallar <nicolas.tallar@iohk.io>
2017-01-11 10:47:56 -0300

    [MPT] Added comments on DataSource trait and Node file functions

--- src/main/scala/io/iohk/ethereum/merklepatriciatrie/Node.scala
+++ src/main/scala/io/iohk/ethereum/merklepatriciatrie/Node.scala
@@ -31,6 +31,14 @@ private[merklepatriciatrie] case class LeafNode(key: Array[Byte], value: Array[B
 private[merklepatriciatrie] case class ExtensionNode(sharedKey: Array[Byte], next: Either[Array[Byte], Node]) extends Node
 
 private[merklepatriciatrie] object ExtensionNode {
+  /**
+    * This function creates a new ExtensionNode with next parameter as its node pointer
+    *
+    * @param sharedKey of the new ExtensionNode.
+    * @param next to be inserted as the node pointer (and hashed if necessary).
+    * @param hashFn to hash the node if necessary.
+    * @return a new BranchNode.
+    */
   def apply(sharedKey: Array[Byte], next: Node, hashFn: HashFn): ExtensionNode = {
     val nextCapped = next.capped(hashFn)
     ExtensionNode(sharedKey, if (nextCapped.length == 32) Left(nextCapped) else Right(next))
@@ -38,6 +46,14 @@ private[merklepatriciatrie] object ExtensionNode {
 }
 
 private[merklepatriciatrie] case class BranchNode(children: Seq[Option[Either[Array[Byte], Node]]], terminator: Option[Array[Byte]]) extends Node {
+  /**
+    * This function creates a new BranchNode by updating one of the children of the self node.
+    *
+    * @param childIndex of the BranchNode children where the child should be inserted.
+    * @param childNode to be inserted as a child of the new BranchNode (and hashed if necessary).
+    * @param hashFn to hash the node if necessary.
+    * @return a new BranchNode.
+    */
   def updateChild(childIndex: Int, childNode: Node, hashFn: HashFn): BranchNode = {
     val childCapped = childNode.capped(hashFn)
     BranchNode(children.updated(childIndex, Some(if (childCapped.length == 32) Left(childCapped) else Right(childNode))), terminator)
@@ -47,14 +63,40 @@ private[merklepatriciatrie] case class BranchNode(children: Seq[Option[Either[Ar
 private[merklepatriciatrie] object BranchNode {
   private val emptyChildren: Seq[Option[Either[Array[Byte], Node]]] = Array.fill(MerklePatriciaTrie.ListSize - 1)(None)
 
+  /**
+    * This function creates a new terminator BranchNode having only a value associated with it.
+    * This new BranchNode will be temporarily in an invalid state.
+    *
+    * @param terminator to be associated with the new BranchNode.
+    * @return a new BranchNode.
+    */
   def withValueOnly(terminator: Array[Byte]): BranchNode =
     BranchNode(emptyChildren, Some(terminator))
 
+  /**
+    * This function creates a new BranchNode having only one child associated with it (and optionaly a value).
+    * This new BranchNode will be temporarily in an invalid state.
+    *
+    * @param position of the BranchNode children where the child should be inserted.
+    * @param child to be inserted as a child of the new BranchNode (and hashed if necessary).
+    * @param terminator to be associated with the new BranchNode.
+    * @param hashFn to hash the node if necessary.
+    * @return a new BranchNode.
+    */
   def withSingleChild(position: Byte, child: Node, terminator: Option[Array[Byte]], hashFn: HashFn): BranchNode = {
     val childCapped = child.capped(hashFn)
     BranchNode(emptyChildren.updated(position, Some(if (childCapped.length == 32) Left(childCapped) else Right(child))), terminator)
   }
 
+  /**
+    * This function creates a new BranchNode having only one child associated with it (and optionaly a value).
+    * This new BranchNode will be temporarily in an invalid state.
+    *
+    * @param position of the BranchNode children where the child should be inserted.
+    * @param child to be inserted as a child of the new BranchNode (already hashed if necessary).
+    * @param terminator to be associated with the new BranchNode.
+    * @return a new BranchNode.
+    */
   def withSingleChild(position: Byte, child: Either[Array[Byte], Node], terminator: Option[Array[Byte]]): BranchNode =
     BranchNode(emptyChildren.updated(position, Some(child)), terminator)
 }
--- src/main/scala/io/iohk/ethereum/merklepatriciatrie/package.scala
+++ src/main/scala/io/iohk/ethereum/merklepatriciatrie/package.scala
@@ -12,8 +12,23 @@ package object merklepatriciatrie {
     type Key = Array[Byte]
     type Value = Array[Byte]
 
+    /**
+      * This function obtains the associated value to a key, if there exists one.
+      *
+      * @param key
+      * @return the value associated with the passed key
+      */
     def get(key: Key): Option[Value]
 
+    /**
+      * This function updates the DataSource by deleting and inserting new (key-value) pairs.
+      *
+      * @param rootHash of the latest version of the trie, after updating it with the (key-value) pairs from toRemove and toUpdate.
+      *                 Its purpose is to be used for version name generation if necessary (as is the case with IODB).
+      * @param toRemove which includes all the keys to be removed from the DataSource.
+      * @param toUpdate which includes all the (key-value) pairs to be inserted into the DataSource.
+      * @return the new DataSource after the removals and insertions were done.
+      */
     def update(rootHash: Array[Byte], toRemove: Seq[Key], toUpdate: Seq[(Key, Value)]): DataSource
   }
 }
