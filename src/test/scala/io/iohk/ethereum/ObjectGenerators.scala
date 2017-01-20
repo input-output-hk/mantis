d4f9aecb0edf205d5a22f1077c22cbd7f114b3bc
Jan <janek.ziniewicz@gmail.com>
2017-01-20 17:21:54 +0100

    Add ByteString generator

--- src/test/scala/io/iohk/ethereum/ObjectGenerators.scala
+++ src/test/scala/io/iohk/ethereum/ObjectGenerators.scala
@@ -1,16 +1,25 @@
 package io.iohk.ethereum
 
+import akka.util.ByteString
 import java.math.BigInteger
 import org.scalacheck.{Arbitrary, Gen}
 
 trait ObjectGenerators {
 
-  lazy val intGen: Gen[Int] = Gen.choose(Int.MinValue, Int.MaxValue)
+  def intGen: Gen[Int] = Gen.choose(Int.MinValue, Int.MaxValue)
 
-  lazy val bigIntGen: Gen[BigInt] = byteArrayOfNItemsGen(32).map(b=>new BigInteger(1, b))
+  def bigIntGen: Gen[BigInt] = byteArrayOfNItemsGen(32).map(b => new BigInteger(1, b))
 
-  lazy val anyArrayGen: Gen[Array[Byte]] = Gen.nonEmptyListOf(Arbitrary.arbitrary[Byte]).map(_.toArray)
+  def byteStringGen(minSize: Int = 0, maxSize: Int = 32): Gen[ByteString] = {
+    for {
+      size <- Gen.choose(minSize, maxSize)
+      byteArray <- byteArrayOfNItemsGen(size)
+    } yield {
+      ByteString(byteArray)
+    }
+  }
 
   def byteArrayOfNItemsGen(n: Int): Gen[Array[Byte]] = Gen.listOfN(n, Arbitrary.arbitrary[Byte]).map(_.toArray)
+
 
 }
