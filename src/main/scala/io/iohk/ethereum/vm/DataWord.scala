ad8d32d104a002e8d7a4990e9dbea93e68266ed6
Jan <janek.ziniewicz@gmail.com>
2017-01-20 14:44:19 +0100

    Create a basic implementation of 256-bit word

--- src/main/scala/io/iohk/ethereum/vm/DataWord.scala
+++ src/main/scala/io/iohk/ethereum/vm/DataWord.scala
@@ -1,12 +1,69 @@
 package io.iohk.ethereum.vm
 
-object DataWord {
-  val MaxWord = BigInt(2).pow(256) - 1
+import akka.util.ByteString
+import scala.math.ScalaNumericConversions
 
-  def fromBigInt(value: BigInt): DataWord =
-    DataWord(if (value < 0) value % MaxWord + MaxWord else value % MaxWord)
+
+object DataWord {
+
+  val MaxLength = 32
+
+  // TODO: Change internal representation of a DataWord to a ByteString. Code below makes 3 obsolete allocations of arrays.
+  private def apply(n: BigInt): DataWord = {
+    DataWord(ByteString((n).toByteArray))
+  }
+
 }
 
-case class DataWord(value: BigInt) {
-  require(value >= 0 && value <= DataWord.MaxWord, s"Invalid word value: $value")
+case class DataWord(value: ByteString) extends ScalaNumericConversions {
+  import DataWord._
+
+  require(value.length <= MaxLength, s"Input byte array  cannot be longer than $MaxLength: ${value.length}")
+
+  private val n: BigInt = value.foldLeft(BigInt(0)) { (n, b) => (n << 8) + (b & 0xff) }
+
+  def &(that: DataWord): DataWord = {
+    DataWord(this.n & that.n)
+  }
+
+  def |(that: DataWord): DataWord = {
+    DataWord(this.n | that.n)
+  }
+
+  def ^(that: DataWord): DataWord = {
+    DataWord(this.n ^ that.n)
+  }
+
+  def unary_-(): DataWord = {
+    DataWord(-this.n)
+  }
+
+  def +(that: DataWord): DataWord = {
+    DataWord(this.n + that.n)
+  }
+
+  def -(that: DataWord): DataWord = {
+    DataWord(this.n - that.n)
+  }
+
+  def *(that: DataWord): DataWord = {
+    DataWord(this.n * that.n)
+  }
+
+  def /(that: DataWord): DataWord = {
+    DataWord(this.n / that.n)
+  }
+
+  def doubleValue(): Double = longValue().toDouble
+
+  def floatValue(): Float = longValue().toFloat
+
+  def intValue(): Int = value.foldLeft(0) { (n, b) => (n << 8) + (b & 0xff) }
+
+  def longValue(): Long = value.foldLeft(0l) { (n, b) => (n << 8) + (b & 0xff) }
+
+  def isWhole(): Boolean = true
+
+  def underlying(): AnyRef = n
+
 }
