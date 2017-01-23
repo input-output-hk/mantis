f6571c975a680bceeed624dbd100760e8bfcea0c
Lukasz Gasior <lukasz.gasior@iohk.io>
2017-01-23 14:44:58 +0100

    Add Logger object

--- src/main/scala/io/iohk/ethereum/utils/Logger.scala
+++ src/main/scala/io/iohk/ethereum/utils/Logger.scala
@@ -5,3 +5,8 @@ import org.slf4j.LoggerFactory
 trait Logger {
   val log = LoggerFactory.getLogger(getClass)
 }
+
+object Logger {
+  def getLogger(clazz: Class[_]): org.slf4j.Logger = LoggerFactory.getLogger(clazz)
+  def getLogger(name: String): org.slf4j.Logger = LoggerFactory.getLogger(name)
+}
