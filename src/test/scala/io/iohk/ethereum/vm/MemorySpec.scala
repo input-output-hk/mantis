713cfbc2baeea0ea289c41c9ca1657c85590191e
Radek Tkaczyk <radek.tkaczyk@iohk.io>
2017-01-24 21:01:09 +0100

    fix MemorySpec

--- src/test/scala/io/iohk/ethereum/vm/MemorySpec.scala
+++ src/test/scala/io/iohk/ethereum/vm/MemorySpec.scala
@@ -12,9 +12,9 @@ class MemorySpec extends FunSuite with PropertyChecks with ObjectGenerators {
   import DataWord._
 
   test("store DataWord") {
-    assert(Memory().store(0, fill(1)).buffer == fill(1).value)
-    assert(Memory().store(2, fill(1)).buffer == ByteString(0, 0) ++ fill(1).value)
-    assert(Memory().store(2, fill(1)).store(1, fill(2)).buffer == ByteString(0) ++ fill(2).value :+ 1)
+    assert(Memory().store(0, fill(1)).buffer == fill(1).bytes)
+    assert(Memory().store(2, fill(1)).buffer == ByteString(0, 0) ++ fill(1).bytes)
+    assert(Memory().store(2, fill(1)).store(1, fill(2)).buffer == ByteString(0) ++ fill(2).bytes :+ 1)
     assert(Memory().store(-1, fill(1)).error == Some(InvalidAddress))
   }
 
@@ -28,6 +28,10 @@ class MemorySpec extends FunSuite with PropertyChecks with ObjectGenerators {
   test("load DataWord") {
     // assert(Memory(ByteString(0, 0) ++ fill(1).value).load(-1).value == fill(0).value)
     // assert(Memory(ByteString(0, 0) ++ fill(1).value).load(1).value == 0 +: ByteString(Array.fill[Byte](31)(1)))
-    assert(Memory(ByteString(0, 0) ++ fill(1).value).load(3).value == 0 +: ByteString(Array.fill[Byte](31)(1)))
+    assert(Memory(ByteString(0, 0) ++ fill(1).bytes).load(3).bytes == 0 +: ByteString(Array.fill[Byte](31)(1)))
   }
+
+  def fill(b: Byte): DataWord =
+    DataWord(ByteString(Array.fill[Byte](MaxLength)(b)))
+
 }
