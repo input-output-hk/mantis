7e3a39d8006cbb55cc6cfabbe9f25cf9d74f37cc
Adam Smolarek <adam.smolarek@iohk.io>
2017-01-27 16:56:44 +0100

    wip

--- src/main/scala/io/iohk/ethereum/network/FastSyncActor.scala
+++ src/main/scala/io/iohk/ethereum/network/FastSyncActor.scala
@@ -4,7 +4,7 @@ import akka.actor.{Actor, ActorLogging, ActorRef, Props, Terminated}
 import akka.util.ByteString
 import io.iohk.ethereum.network.FastSyncActor.{PartialDownloadDone, StartSync}
 import io.iohk.ethereum.network.p2p.messages.PV62.{BlockBodies, BlockHeaders, GetBlockBodies, GetBlockHeaders}
-import io.iohk.ethereum.network.p2p.messages.PV63.{GetReceipts, Receipts}
+import io.iohk.ethereum.network.p2p.messages.PV63._
 
 class FastSyncActor(peerActor: ActorRef) extends Actor with ActorLogging {
 
@@ -13,6 +13,9 @@ class FastSyncActor(peerActor: ActorRef) extends Actor with ActorLogging {
   var currentBlockHeaders: Option[BlockHeaders] = None
   var currentBlockBodies: Option[BlockBodies] = None
   var currentReceipts: Option[Receipts] = None
+
+  var currentNodeHash: Option[Seq[ByteString]] = None
+  var mptNodes: Map[ByteString, Either[MptNode, ByteString]] = Map()
 
   def handleTerminated: Receive = {
     case _: Terminated =>
@@ -23,6 +26,8 @@ class FastSyncActor(peerActor: ActorRef) extends Actor with ActorLogging {
   override def receive: Receive = handleTerminated orElse {
     case StartSync(startBlockHash, targetBlockHash) =>
       peerActor ! PeerActor.SendMessage(GetBlockHeaders(Right(startBlockHash), blocksPerMessage, 0, reverse = false))
+      peerActor ! PeerActor.SendMessage(GetNodeData(Seq(targetBlockHash)))
+      currentNodeHash = Some(Seq(targetBlockHash))
 
     case m: BlockHeaders =>
       currentBlockHeaders = Some(m)
@@ -43,6 +48,23 @@ class FastSyncActor(peerActor: ActorRef) extends Actor with ActorLogging {
           log.info("got complete info \n{}\n{}\n{}", headers, bodies, receipts)
 
         case _ =>
+      }
+
+    case m: NodeData =>
+      val v: Seq[(ByteString, Either[MptNode, ByteString])] = m.values.zip(currentNodeHash.get).map { case (value, hash) => hash -> value }
+      mptNodes ++ Map[ByteString, Either[MptNode, ByteString]](v: _*)
+      m.values.collect { case Left(mptNode) => mptNode }.map {
+        case n: MptLeaf =>
+
+        case n: MptBranch =>
+          val hashes = n.children.collect { case Left(MptHash(hash)) => hash }
+          currentNodeHash = Some(hashes)
+          peerActor ! PeerActor.SendMessage(GetNodeData(hashes))
+        case n: MptExtension =>
+          n.child.fold({ a =>
+            currentNodeHash = Some(Seq(a.hash))
+            peerActor ! PeerActor.SendMessage(GetNodeData(Seq(a.hash)))
+          }, _)
       }
   }
 }
