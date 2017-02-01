40b9f26913f0679783fab01cb67ec9942accccfa
Jan <janek.ziniewicz@gmail.com>
2017-02-01 12:47:54 +0100

    Remove magic number

--- src/test/scala/io/iohk/ethereum/vm/StorageSpec.scala
+++ src/test/scala/io/iohk/ethereum/vm/StorageSpec.scala
@@ -7,8 +7,10 @@ import org.scalatest.prop.PropertyChecks
 
 class StorageSpec extends FunSuite with PropertyChecks {
 
+  val MaxStorageSize = 10
+
   test("store") {
-    forAll(getStorageGen(10), getDataWordGen(), getDataWordGen()) {(storage: Storage, k: DataWord, v: DataWord) =>
+    forAll(getStorageGen(MaxStorageSize), getDataWordGen(), getDataWordGen()) {(storage: Storage, k: DataWord, v: DataWord) =>
       val updatedStorage: Storage = storage.store(k, v)
       assert(updatedStorage.toMap == storage.toMap + (k -> v))
       whenever(!storage.toMap.isEmpty) {
@@ -20,13 +22,13 @@ class StorageSpec extends FunSuite with PropertyChecks {
   }
 
   test("load") {
-    forAll(getStorageGen(10), getDataWordGen(), getDataWordGen()) {(storage: Storage, k: DataWord, v: DataWord) =>
+    forAll(getStorageGen(MaxStorageSize), getDataWordGen(), getDataWordGen()) {(storage: Storage, k: DataWord, v: DataWord) =>
         assert(storage.store(k, v).load(k) == v)
     }
   }
 
   test("==") {
-    forAll(getListGen(0, 10, getDataWordGen())) {(dataWords: Seq[DataWord]) =>
+    forAll(getListGen(0, MaxStorageSize, getDataWordGen())) {(dataWords: Seq[DataWord]) =>
       assert(Storage.fromSeq(dataWords) == Storage.fromSeq(dataWords))
     }
   }
