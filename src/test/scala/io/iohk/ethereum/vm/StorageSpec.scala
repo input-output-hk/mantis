72e2b34db1c694708031db1feee01eff5fa1c220
Jan <janek.ziniewicz@gmail.com>
2017-02-01 11:11:25 +0100

    Add tests for Storage class.

---
+++ src/test/scala/io/iohk/ethereum/vm/StorageSpec.scala
@@ -0,0 +1,36 @@
+package io.iohk.ethereum.vm
+
+import akka.util.ByteString
+import io.iohk.ethereum.vm.Generators._
+import org.scalatest.FunSuite
+import org.scalatest.prop.PropertyChecks
+import io.iohk.ethereum.vm.DataWord._
+
+
+class StorageSpec extends FunSuite with PropertyChecks {
+
+  test("store") {
+    forAll(getStorageGen(10), getDataWordGen(), getDataWordGen()) {(storage: Storage, k: DataWord, v: DataWord) =>
+      val updatedStorage: Storage = storage.store(k, v)
+      assert(updatedStorage.toMap == storage.toMap + (k -> v))
+      whenever(!storage.toMap.isEmpty) {
+        storage.toMap.headOption.foreach { case (k, v) =>
+          assert(storage.store(k, v) == storage)
+        }
+      }
+    }
+  }
+
+  test("load") {
+    forAll(getStorageGen(10), getDataWordGen(), getDataWordGen()) {(storage: Storage, k: DataWord, v: DataWord) =>
+        assert(storage.store(k, v).load(k) == v)
+    }
+  }
+
+  test("==") {
+    forAll(getListGen(0, 10, getDataWordGen())) {(dataWords: Seq[DataWord]) =>
+      assert(Storage.fromSeq(dataWords) == Storage.fromSeq(dataWords))
+    }
+  }
+
+}
