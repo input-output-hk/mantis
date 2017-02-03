54862c4a332694a8bf02f1815486954952a4cbcf
Nicolas Tallar <nicolas.tallar@iohk.io>
2017-02-03 11:07:07 -0300

    [KeyValueStorage] NodeStorage tests

--- src/test/scala/io/iohk/ethereum/db/storage/NodeStorageSuite.scala
+++ src/test/scala/io/iohk/ethereum/db/storage/NodeStorageSuite.scala
@@ -5,34 +5,37 @@ import io.iohk.ethereum.db.dataSource.EphemDataSource
 import org.scalacheck.Gen
 import org.scalatest.FunSuite
 import org.scalatest.prop.PropertyChecks
+import io.iohk.ethereum.rlp.{encode => rlpEncode, decode => rlpDecode}
+import io.iohk.ethereum.network.p2p.messages.PV63.MptNode
 
 import scala.util.Random
 
-class MptNodeStorageSuite extends FunSuite with PropertyChecks with ObjectGenerators {
-  test("MptNodeStorage insert") {
+class NodeStorageSuite extends FunSuite with PropertyChecks with ObjectGenerators {
+  test("NodeStorage insert") {
     forAll(Gen.listOf(nodeGen)){ unfilteredMptNodes =>
       val mptNodes = unfilteredMptNodes.distinct
-      val initialNodeStorage = new MptNodeStorage(EphemDataSource())
+      val initialNodeStorage = new NodeStorage(EphemDataSource())
       val nodeStorage = mptNodes.foldLeft(initialNodeStorage){
         case (recNodeStorage, node) =>
-          recNodeStorage.put(node)
+          recNodeStorage.put(node.hash.toArray, rlpEncode(node))
       }
 
       mptNodes.foreach{ node =>
-        assert(nodeStorage.get(node.hash).contains(node))
+        val obtainedNode = nodeStorage.get(node.hash.toArray).map(rlpDecode[MptNode])
+        assert(obtainedNode.contains(node))
       }
     }
   }
 
-  test("MptNodeStorage delete") {
+  test("NodeStorage delete") {
     forAll(Gen.listOf(nodeGen)){ unfilteredMptNodes =>
       val mptNodes = unfilteredMptNodes.distinct
 
       //Nodes are inserted
-      val initialNodeStorage = new MptNodeStorage(EphemDataSource())
+      val initialNodeStorage = new NodeStorage(EphemDataSource())
       val nodeStorage = mptNodes.foldLeft(initialNodeStorage){
         case (recNodeStorage, node) =>
-          recNodeStorage.put(node)
+          recNodeStorage.put(node.hash.toArray, rlpEncode(node))
       }
 
       //Nodes are deleted
@@ -40,11 +43,13 @@ class MptNodeStorageSuite extends FunSuite with PropertyChecks with ObjectGenera
         .splitAt(Gen.choose(0, mptNodes.size).sample.get)
       val nodeStorageAfterDelete = toDelete.foldLeft(nodeStorage){
         case (recNodeStorage, node) =>
-          recNodeStorage.remove(node.hash)
+          recNodeStorage.remove(node.hash.toArray)
       }
 
-      toLeave.foreach{ node => assert(nodeStorageAfterDelete.get(node.hash).contains(node)) }
-      toDelete.foreach{ node => assert(nodeStorageAfterDelete.get(node.hash).isEmpty) }
+      toLeave.foreach{ node =>
+        val obtainedNode = nodeStorageAfterDelete.get(node.hash.toArray).map(rlpDecode[MptNode])
+        assert(obtainedNode.contains(node)) }
+      toDelete.foreach{ node => assert(nodeStorageAfterDelete.get(node.hash.toArray).isEmpty) }
     }
   }
 }
