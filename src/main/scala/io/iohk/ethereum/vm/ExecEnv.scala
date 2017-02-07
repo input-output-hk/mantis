62caae22c6da1b2c448e24ac19eb36d3a592cbdc
Jan <janek.ziniewicz@gmail.com>
2017-02-07 15:23:49 +0100

    Update comment.

--- src/main/scala/io/iohk/ethereum/vm/ExecEnv.scala
+++ src/main/scala/io/iohk/ethereum/vm/ExecEnv.scala
@@ -3,7 +3,9 @@ package io.iohk.ethereum.vm
 import akka.util.ByteString
 import io.iohk.ethereum.domain.Address
 
-/** Execution environment constants of an EVM program */
+/** Execution environment constants of an EVM program.
+ *  See section 9.3 in Yellow Paper for more detail.
+*/
 case class ExecEnv(ownerAddr: Address,
                    senderAddr: Address,
                    gasPrice: BigInt,
