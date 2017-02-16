5ab4d47aec92cd4c8427f83c8788a1b522d8851e
Alan Verbner <alan.verbner@iohk.io>
2017-02-16 19:36:01 -0300

    [EC-37] Improved tests

--- src/test/scala/io/iohk/ethereum/blockchain/BlockchainSpec.scala
+++ src/test/scala/io/iohk/ethereum/blockchain/BlockchainSpec.scala
@@ -6,39 +6,33 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class BlockchainSpec extends FlatSpec with Matchers {
 
-  def withBlockchain(testCode: Blockchain => Any): Unit = {
-    val storagesImpl = new Storages.DefaultStorages with SharedEphemDataSources
-    val blockchainComp: BlockchainComp = new BlockchainCompImpl {
+  "Blockchain" should "be able to store a block and return if if queried by hash" in new BlockchainTestSetup {
+    val validBlock = Fixtures.Blocks.ValidBlock.block
+    blockchain.save(validBlock)
+    val block = blockchain.getBlockByHash(validBlock.header.hash)
+    assert(block.isDefined)
+    assert(validBlock == block.get)
+  }
+
+  "Blockchain" should "be able to store a block and retrieve it by number" in new BlockchainTestSetup {
+    val validBlock = Fixtures.Blocks.ValidBlock.block
+    blockchain.save(validBlock)
+    val block = blockchain.getBlockByNumber(validBlock.header.number)
+    assert(block.isDefined)
+    assert(validBlock == block.get)
+  }
+
+  "Blockchain" should "not return a value if not stored" in new BlockchainTestSetup {
+    assert(blockchain.getBlockByNumber(Fixtures.Blocks.ValidBlock.header.number).isEmpty)
+    assert(blockchain.getBlockByHash(Fixtures.Blocks.ValidBlock.header.hash).isEmpty)
+  }
+
+  trait BlockchainTestSetup {
+    private val storagesImpl = new Storages.DefaultStorages with SharedEphemDataSources
+    private val blockchainComp: BlockchainComp = new BlockchainCompImpl {
       override val storagesComp: StoragesComp = storagesImpl
     }
-    testCode(blockchainComp.blockchain)
-  }
-
-  "Blockchain" should "be able to store a block and return if if queried by hash" in {
-    withBlockchain { blockchain =>
-      val validBlock = Fixtures.Blocks.ValidBlock.block
-      blockchain.save(validBlock)
-      val block = blockchain.getBlockByHash(validBlock.header.hash)
-      assert(block.isDefined)
-      assert(validBlock == block.get)
-    }
-  }
-
-  "Blockchain" should "be able to store a block and retrieve it by number" in {
-    withBlockchain { blockchain =>
-      val validBlock = Fixtures.Blocks.ValidBlock.block
-      blockchain.save(validBlock)
-      val block = blockchain.getBlockByNumber(validBlock.header.number)
-      assert(block.isDefined)
-      assert(validBlock == block.get)
-    }
-  }
-
-  "Blockchain" should "not return a value if not stored" in {
-    withBlockchain { blockchain =>
-      assert(blockchain.getBlockByNumber(Fixtures.Blocks.ValidBlock.header.number).isEmpty)
-      assert(blockchain.getBlockByHash(Fixtures.Blocks.ValidBlock.header.hash).isEmpty)
-    }
+    val blockchain: Blockchain = blockchainComp.blockchain
   }
 
 }
--- src/test/scala/io/iohk/ethereum/rpc/BlockControllerSpec.scala
+++ src/test/scala/io/iohk/ethereum/rpc/BlockControllerSpec.scala
@@ -11,57 +11,50 @@ import io.iohk.ethereum.domain.Block
 import io.iohk.ethereum.rpc.BlockController.BlockView
 import org.scalatest.{FlatSpec, Matchers}
 
-class BlockControllerSpec extends FlatSpec with Matchers with ScalatestRouteTest {
-
-  def withRoute(testCode: Route => Any): Unit = {
-    testCode(BlockController.route(stubbedBlockchain))
-  }
+class BlockControllerSpec extends FlatSpec with Matchers with ScalatestRouteTest with BlockRouteSetup {
 
   "BlockController" should "return the block when queried by block number" in {
-    withRoute { route =>
-      val blockHeader = Fixtures.Blocks.ValidBlock.header
-      generateRequest("getBlockByNumber", blockHeader.number) ~> route ~> check {
-        status.isSuccess()
-        assert(responseAs[BlockView] equals BlockView(
-          difficulty = blockHeader.difficulty,
-          extraData = blockHeader.extraData,
-          gasLimit = blockHeader.gasLimit,
-          gasUsed = blockHeader.gasUsed,
-          hash = blockHeader.hash,
-          logsBloom = blockHeader.logsBloom,
-          beneficiary = blockHeader.beneficiary,
-          mixHash = blockHeader.mixHash,
-          nonce = blockHeader.nonce,
-          number = blockHeader.number,
-          parentHash = blockHeader.parentHash,
-          receiptsRoot = blockHeader.receiptsRoot,
-          ommersHash = blockHeader.ommersHash,
-          stateRoot = blockHeader.stateRoot,
-          unixTimestamp = blockHeader.unixTimestamp,
-          transactions = Fixtures.Blocks.ValidBlock.transactionHashes,
-          transactionsRoot = blockHeader.transactionsRoot,
-          uncles = Seq(),
-          size = Fixtures.Blocks.ValidBlock.size
-        ))
-      }
+    val blockHeader = Fixtures.Blocks.ValidBlock.header
+    generateRequest("getBlockByNumber", blockHeader.number) ~> route ~> check {
+      status.isSuccess()
+      assert(responseAs[BlockView] equals BlockView(
+        difficulty = blockHeader.difficulty,
+        extraData = blockHeader.extraData,
+        gasLimit = blockHeader.gasLimit,
+        gasUsed = blockHeader.gasUsed,
+        hash = blockHeader.hash,
+        logsBloom = blockHeader.logsBloom,
+        beneficiary = blockHeader.beneficiary,
+        mixHash = blockHeader.mixHash,
+        nonce = blockHeader.nonce,
+        number = blockHeader.number,
+        parentHash = blockHeader.parentHash,
+        receiptsRoot = blockHeader.receiptsRoot,
+        ommersHash = blockHeader.ommersHash,
+        stateRoot = blockHeader.stateRoot,
+        unixTimestamp = blockHeader.unixTimestamp,
+        transactions = Fixtures.Blocks.ValidBlock.transactionHashes,
+        transactionsRoot = blockHeader.transactionsRoot,
+        uncles = Seq(),
+        size = Fixtures.Blocks.ValidBlock.size
+      ))
     }
   }
 
   "BlockController" should "return an error when queried by block number that doesn't exist" in {
-    withRoute { route =>
-      generateRequest("getBlockByNumber", Fixtures.Blocks.ValidBlock.header.number + 1) ~> route ~> check {
-        status == StatusCodes.NotFound
-      }
+    generateRequest("getBlockByNumber", Fixtures.Blocks.ValidBlock.header.number + 1) ~> route ~> check {
+      status == StatusCodes.NotFound
     }
   }
+}
 
-  private def generateRequest(url: String, blockId: BigInt): HttpRequest =
-    HttpRequest(HttpMethods.GET, uri = s"/$url/$blockId")
+trait BlockRouteSetup {
+  def generateRequest(url: String, blockId: BigInt): HttpRequest = HttpRequest(HttpMethods.GET, uri = s"/$url/$blockId")
 
-  private def stubbedBlockchain: Blockchain = new Blockchain {
+  private val stubbedBlockchain: Blockchain = new Blockchain {
 
     override def getBlockByNumber(number: BigInt): Option[Block] =
-      if (number === Fixtures.Blocks.ValidBlock.header.number) Some(Fixtures.Blocks.ValidBlock.block)
+      if (number == Fixtures.Blocks.ValidBlock.header.number) Some(Fixtures.Blocks.ValidBlock.block)
       else None
 
     override def receiptStorage = ???
@@ -74,4 +67,6 @@ class BlockControllerSpec extends FlatSpec with Matchers with ScalatestRouteTest
 
     override def blockNumberMappingStorage: BlockNumberMappingStorage = ???
   }
+
+  val route: Route = BlockController.route(stubbedBlockchain)
 }
