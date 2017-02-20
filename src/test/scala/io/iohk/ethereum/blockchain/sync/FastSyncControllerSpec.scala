844f5a1fdb72832da694b6f2b4e9691e68c8dd03
Lukasz Gasior <lukasz.gasior@iohk.io>
2017-02-20 17:59:52 +0100

    Add tests for fast sync finish

--- src/test/scala/io/iohk/ethereum/blockchain/sync/FastSyncControllerSpec.scala
+++ src/test/scala/io/iohk/ethereum/blockchain/sync/FastSyncControllerSpec.scala
@@ -5,7 +5,7 @@ import java.net.InetSocketAddress
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{Terminated, ActorSystem, Props}
 import akka.agent.Agent
 import akka.testkit.{TestProbe, TestActorRef}
 import akka.util.ByteString
@@ -16,10 +16,11 @@ import io.iohk.ethereum.db.storage._
 import io.iohk.ethereum.domain.BlockHeader
 import io.iohk.ethereum.network.PeerActor
 import io.iohk.ethereum.network.PeerManagerActor.{Peer, PeersResponse, GetPeers}
-import io.iohk.ethereum.network.p2p.messages.PV62.{GetBlockHeaders, BlockHeaders}
-import io.iohk.ethereum.network.p2p.messages.PV63.{GetNodeData, NodeData}
+import io.iohk.ethereum.network.p2p.messages.PV62._
+import io.iohk.ethereum.network.p2p.messages.PV63.{Receipts, GetReceipts, GetNodeData, NodeData}
 import io.iohk.ethereum.utils.{BlockchainStatus, ServerStatus, NodeStatus}
 import org.scalatest.{Matchers, FlatSpec}
+import org.spongycastle.util.encoders.Hex
 
 class FastSyncControllerSpec extends FlatSpec with Matchers {
 
@@ -145,6 +146,63 @@ class FastSyncControllerSpec extends FlatSpec with Matchers {
     // peer should not be blacklisted anymore
     peer.expectMsg(PeerActor.SendMessage(GetNodeData(Seq(targetBlockHeader.stateRoot))))
     peer.expectMsg(PeerActor.Subscribe(Set(NodeData.code)))
+  }
+
+  it should "stop syncing when chain and mpt is downloaded" in new TestSetup {
+    val peer = TestProbe()(system)
+
+    val targetBlockHeader = baseBlockHeader.copy(
+      number = 6,
+      stateRoot = ByteString(Hex.decode("deae1dfad5ec8dcef15915811e1f044d2543674fd648f94345231da9fc2646cc")))
+
+    nodeStatusHolder.send(_.copy(blockchainStatus = BlockchainStatus(
+      10,
+      ByteString("bestHash"),
+      5)))
+
+    time.advance(1.seconds)
+
+    peerManager.expectMsg(GetPeers)
+    peerManager.reply(PeersResponse(Seq(Peer(new InetSocketAddress("127.0.0.1", 0), peer.ref))))
+
+    peer.expectMsg(PeerActor.GetStatus)
+    peer.reply(PeerActor.StatusResponse(PeerActor.Status.Handshaked))
+
+    fastSyncController ! FastSyncController.StartFastSync(targetBlockHeader.hash)
+
+    peer.expectMsg(PeerActor.Subscribe(Set(BlockHeaders.code)))
+    peer.expectMsg(PeerActor.SendMessage(GetBlockHeaders(Right(targetBlockHeader.hash), 1, 0, reverse = false)))
+
+    peer.reply(PeerActor.MessageReceived(BlockHeaders(Seq(targetBlockHeader))))
+    peer.expectMsg(PeerActor.Unsubscribe)
+
+    peer.expectMsg(PeerActor.SendMessage(GetBlockHeaders(Left(6), 10, 0, false)))
+    peer.expectMsg(PeerActor.Subscribe(Set(BlockHeaders.code)))
+    peer.reply(PeerActor.MessageReceived(BlockHeaders(Seq(targetBlockHeader))))
+    peer.expectMsg(PeerActor.Unsubscribe)
+
+    peer.expectMsg(PeerActor.SendMessage(GetReceipts(Seq(targetBlockHeader.hash))))
+    peer.expectMsg(PeerActor.Subscribe(Set(Receipts.code)))
+    peer.reply(PeerActor.MessageReceived(Receipts(Seq(Seq()))))
+    peer.expectMsg(PeerActor.Unsubscribe)
+
+    peer.expectMsg(PeerActor.SendMessage(GetBlockBodies(Seq(targetBlockHeader.hash))))
+    peer.expectMsg(PeerActor.Subscribe(Set(BlockBodies.code)))
+    peer.reply(PeerActor.MessageReceived(BlockBodies(Seq(BlockBody(Nil, Nil)))))
+    peer.expectMsg(PeerActor.Unsubscribe)
+
+    val stateMptLeafWithAccount =
+      ByteString(Hex.decode("f86d9e328415c225a782bb339b22acad1c739e42277bc7ef34de3623114997ce78b84cf84a0186cb7d8738d800a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421a0c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"))
+
+    val watcher = TestProbe()
+    watcher.watch(fastSyncController)
+
+    peer.expectMsg(PeerActor.SendMessage(GetNodeData(Seq(targetBlockHeader.stateRoot))))
+    peer.expectMsg(PeerActor.Subscribe(Set(NodeData.code)))
+    peer.reply(PeerActor.MessageReceived(NodeData(Seq(stateMptLeafWithAccount))))
+    peer.expectMsg(PeerActor.Unsubscribe)
+
+    watcher.expectMsgPF(2.seconds) { case Terminated(`fastSyncController`) => () }
   }
 
   trait TestSetup {
