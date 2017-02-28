148c595a63dc7f39c900d1a7418c0b43a2a2186c
Radek Tkaczyk <radek.tkaczyk@iohk.io>
2017-02-28 12:16:20 +0100

    provided comments for WorldStateProxy; deleted obsolete class

--- src/main/scala/io/iohk/ethereum/vm/Transfer.scala
+++
@@ -1,5 +0,0 @@
-package io.iohk.ethereum.vm
-
-import io.iohk.ethereum.domain.Address
-
-case class Transfer(from: Address, to: Address, value: BigInt)
--- src/main/scala/io/iohk/ethereum/vm/WorldStateProxy.scala
+++ src/main/scala/io/iohk/ethereum/vm/WorldStateProxy.scala
@@ -7,6 +7,12 @@ import io.iohk.ethereum.rlp.RLPList
 import io.iohk.ethereum.rlp.RLPImplicits._
 import io.iohk.ethereum.crypto.kec256
 
+/**
+  * This is a single entry point to all VM interactions with the persisted state. Implementations are meant to be
+  * immutable so that rolling back a transaction is equivalent to discarding resulting changes. The changes to state
+  * should be kept in memory and applied only after a transaction completes without errors. This does not forbid mutable
+  * caches for DB retrieval operations.
+  */
 trait WorldStateProxy {
   type WS <: WorldStateProxy
 
@@ -19,7 +25,12 @@ trait WorldStateProxy {
   def saveStorage(storageRoot: ByteString, storage: Storage): WS
 
 
-  //TODO: provide rationale
+  /**
+    * In certain situation an account is guaranteed to exist, e.g. the account that executes the code, the account that
+    * transfer value to another. There could be no input to our application that would cause this fail, so we shouldn't
+    * handle account existence in such cases. If it does fail, it means there's something terribly wrong with our code
+    * and throwing an exception is an appropriate response.
+    */
   def getGuaranteedAccount(address: Address): Account =
     getAccount(address).get
 
