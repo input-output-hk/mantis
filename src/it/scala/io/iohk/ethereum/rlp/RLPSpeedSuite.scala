717c032ec819f3af1988e3dadc3e294c5bd1f8e6
alan mcsherry <alan.mcsherry@iohk.io>
2017-03-02 17:06:22 +0000

    it tests and scalastyle.
    Signed-off-by: alan mcsherry <alan.mcsherry@iohk.io>

--- src/it/scala/io/iohk/ethereum/rlp/RLPSpeedSuite.scala
+++ src/it/scala/io/iohk/ethereum/rlp/RLPSpeedSuite.scala
@@ -3,7 +3,8 @@ package io.iohk.ethereum.rlp
 import akka.util.ByteString
 import io.iohk.ethereum.ObjectGenerators
 import io.iohk.ethereum.domain.{BlockHeader, SignedTransaction, Transaction}
-import io.iohk.ethereum.rlp
+import io.iohk.ethereum.network.protocol.CommonMessages.SignedTransactions
+import io.iohk.ethereum.network.protocol.PV62.BlockHeaderImplicits._
 import io.iohk.ethereum.utils.Logger
 import io.iohk.ethereum.vm.Address
 import org.scalacheck.Gen
