9e7e7de49aa232bddea11e9c5d715bd61043c509
Radek Tkaczyk <radek.tkaczyk@iohk.io>
2017-03-06 15:35:24 +0100

    printing bytecode with hex offsets

--- src/main/scala/io/iohk/ethereum/vmrunner/Interpreter.scala
+++ src/main/scala/io/iohk/ethereum/vmrunner/Interpreter.scala
@@ -159,17 +159,21 @@ object Interpreter {
   def printCode(code: ByteString, i: Int = 0): String = {
     if (i >= code.size)
       ""
-    else OpCode.byteToOpCode.get(code(i)) match {
-      case Some(op: PushOp) =>
-        val skip = op.code - PUSH1.code + 1
-        val data = code.slice(i + 1, i + skip + 1).map(b => f"$b%02x").mkString(" ")
-        s"  $op $data\n" + printCode(code, i + skip + 1)
+    else {
+      val opcode = OpCode.byteToOpCode.get(code(i)) match {
+        case Some(op: PushOp) =>
+          val skip = op.code - PUSH1.code + 1
+          val data = code.slice(i + 1, i + skip + 1).map(b => f"$b%02x").mkString(" ")
+          s"$op $data\n" + printCode(code, i + skip + 1)
 
-      case Some(op) =>
-        s"  $op\n" + printCode(code, i + 1)
+        case Some(op) =>
+          s"$op\n" + printCode(code, i + 1)
 
-      case None =>
-        f"  0x${code(i)}%02x\n" + printCode(code, i + 1)
+        case None =>
+          f"0x${code(i)}%02x\n" + printCode(code, i + 1)
+      }
+
+      f"  $i%04x:    " + opcode
     }
   }
 
