4533b215dc5dcf373834817692861b40335d917a
Adam Smolarek <adam.smolarek@iohk.io>
2017-03-07 12:18:08 +0100

    more 'Scala style guide' like

--- src/test/scala/io/iohk/ethereum/vm/MockWorldState.scala
+++ src/test/scala/io/iohk/ethereum/vm/MockWorldState.scala
@@ -29,11 +29,11 @@ case class MockWorldState(
   def getStorage(address: Address): MockStorage =
     storages.getOrElse(address, MockStorage.Empty)
 
-  def getBlockHash(number: BigInt): Option[ByteString] = if (numberOfHashes >= number && number >= 0) {
-    Some(ByteString(kec256(number.toByteArray)))
-  } else {
-    None
-  }
+  def getBlockHash(number: BigInt): Option[ByteString] =
+    if (numberOfHashes >= number && number >= 0)
+      Some(ByteString(kec256(number.toByteArray)))
+    else
+      None
 
   def saveCode(address: Address, code: ByteString): MockWorldState =
     copy(codeRepo = codeRepo + (address -> code))
