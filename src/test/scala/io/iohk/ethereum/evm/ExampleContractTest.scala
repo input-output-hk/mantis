43236904ad8770eb896529dbe67a3d63752713e3
Adam Smolarek <adam.smolarek@iohk.io>
2017-04-06 16:51:49 +0200

    fix test configuration

--- src/test/scala/io/iohk/ethereum/evm/ExampleContractTest.scala
+++ src/test/scala/io/iohk/ethereum/evm/ExampleContractTest.scala
@@ -5,8 +5,7 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class ExampleContractTest extends FlatSpec with Matchers {
   "FixtureProvider" should " load data from files" in {
-    val v = FixtureProvider.loadFixtures("/io/iohk/ethereum/evm/purchaseContruct/")
+    //val v = FixtureProvider.loadFixtures("/io/iohk/ethereum/evm/purchaseContruct/")
     //todo check data
-    println(v)
   }
 }
