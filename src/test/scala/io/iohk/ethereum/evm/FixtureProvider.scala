b507a315e2d55f9b9d438fdb290c9dd44a20b583
Adam Smolarek <adam.smolarek@iohk.io>
2017-04-06 14:24:35 +0200

    wip

--- src/test/scala/io/iohk/ethereum/evm/FixtureProvider.scala
+++ src/test/scala/io/iohk/ethereum/evm/FixtureProvider.scala
@@ -1,5 +1,22 @@
 package io.iohk.ethereum.evm
 
-class FixtureProvider {
+import akka.util.ByteString
+import io.iohk.ethereum.domain.BlockHeader
+import io.iohk.ethereum.network.p2p.messages.PV62.BlockBody
+import io.iohk.ethereum.network.p2p.messages.PV63.{MptNode, Receipts}
 
+object FixtureProvider {
+
+  case class Fixture(
+    blockHeaders: Map[ByteString, BlockHeader],
+    blockBodies: Map[ByteString, BlockBody],
+    receipts: Map[ByteString, Receipts],
+    stateMpt: Map[ByteString, MptNode],
+    contractMpts: Map[ByteString, MptNode],
+    evmCode: Map[ByteString, ByteString]
+  )
+
+  def loadFixtures(path: String): Unit = {
+
+  }
 }
