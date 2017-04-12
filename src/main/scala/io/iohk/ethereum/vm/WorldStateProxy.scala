7ce2ddfcea427a6e7521d696650cf701c1dc3f60
Nicolas Tallar <nicolas.tallar@iohk.io>
2017-04-12 10:21:34 -0300

    Fixed transfer in WorldState with same origin and destination

--- src/main/scala/io/iohk/ethereum/vm/WorldStateProxy.scala
+++ src/main/scala/io/iohk/ethereum/vm/WorldStateProxy.scala
@@ -14,7 +14,7 @@ import io.iohk.ethereum.rlp.RLPList
   * should be kept in memory and applied only after a transaction completes without errors. This does not forbid mutable
   * caches for DB retrieval operations.
   */
-trait WorldStateProxy[WS <: WorldStateProxy[WS, S], S <: Storage[S]] {
+trait WorldStateProxy[WS <: WorldStateProxy[WS, S], S <: Storage[S]] { self: WS =>
 
   protected def getAccount(address: Address): Option[Account]
   protected def saveAccount(address: Address, account: Account): WS
@@ -46,9 +46,12 @@ trait WorldStateProxy[WS <: WorldStateProxy[WS, S], S <: Storage[S]] {
     getAccount(address).map(a => UInt256(a.balance)).getOrElse(UInt256.Zero)
 
   def transfer(from: Address, to: Address, value: UInt256): WS = {
-    val debited = getGuaranteedAccount(from).increaseBalance(-value)
-    val credited = getAccount(to).getOrElse(Account.Empty).increaseBalance(value)
-    saveAccount(from, debited).saveAccount(to, credited)
+    if(from == to) this
+    else {
+      val debited = getGuaranteedAccount(from).increaseBalance(-value)
+      val credited = getAccount(to).getOrElse(Account.Empty).increaseBalance(value)
+      saveAccount(from, debited).saveAccount(to, credited)
+    }
   }
 
   def newAddress(creatorAddr: Address): (Address, WS) = {
