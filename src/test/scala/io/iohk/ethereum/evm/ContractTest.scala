5b54f7698d2d04654e648e67b2a1c03b96854dba
Adam Smolarek <adam.smolarek@iohk.io>
2017-04-12 13:55:12 +0200

    failing test for Ledger

--- src/test/scala/io/iohk/ethereum/evm/ContractTest.scala
+++ src/test/scala/io/iohk/ethereum/evm/ContractTest.scala
@@ -7,7 +7,7 @@ import org.scalatest.{FlatSpec, Matchers}
 import scala.language.postfixOps
 
 class ContractTest extends FlatSpec with Matchers {
-  "FixtureProvider" should " load data from files" in {
+  "Ledger" should "transfer ether" in {
     val fixtures: FixtureProvider.Fixture = FixtureProvider.loadFixtures("/evm_test/purchaseContruct/")
 
     val (storage, stateStorage) = FixtureProvider.prepareStorages(0, fixtures)
@@ -18,5 +18,14 @@ class ContractTest extends FlatSpec with Matchers {
     //block 2 contains contract creation
     //block 3 and 4 contains contract calls with contract storage modification
     //block 4 contains contract paying to 2 accounts
+  }
+
+  ignore should "execute contract call" in {
+    val fixtures: FixtureProvider.Fixture = FixtureProvider.loadFixtures("/evm_test/purchaseContruct/")
+
+    val (storage, stateStorage) = FixtureProvider.prepareStorages(2, fixtures)
+
+    //block only with ether transfers
+    Ledger.executeBlock(fixtures.blockByNumber(3), storage, stateStorage)
   }
 }
