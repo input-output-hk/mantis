cddd1c260efc73d90c5d5fe03f582d2e8194208e
Radek Tkaczyk <radek.tkaczyk@iohk.io>
2017-04-24 10:12:32 +0200

    fix memory expansion

--- src/main/scala/io/iohk/ethereum/vm/Memory.scala
+++ src/main/scala/io/iohk/ethereum/vm/Memory.scala
@@ -24,28 +24,23 @@ class Memory(private[vm] val underlying: ByteString) {
 
   def store(offset: UInt256, b: Byte): Memory = store(offset, ByteString(b))
 
-  def store(offset: UInt256, dw: UInt256): Memory = store(offset, dw.bytes)
+  def store(offset: UInt256, uint: UInt256): Memory = store(offset, uint.bytes)
 
   def store(offset: UInt256, bytes: Array[Byte]): Memory = store(offset, ByteString(bytes))
 
-  /** Stores a ByteString under the given address.
-   * Underlying byte array is expanded if a ByteString doesn't fit into it.
-   * All empty cells of an expanded array are set to 0.
-   * This method may throw OOM.
-   */
-  def store(offset: UInt256, bs: ByteString): Memory = {
+  /** Stores data at the given offset.
+    * The memory is automatically expanded to accommodate new data - filling empty regions with zeroes if necessary -
+    * hence an OOM error may be thrown.
+    */
+  def store(offset: UInt256, data: ByteString): Memory = {
     val idx: Int = offset.toInt
-    val newUnderlying: ByteString = if (idx + bs.length <= underlying.length) {
-      // a new buffer fits into an old buffer
-      val (prepending, following) = underlying.splitAt(idx)
-      prepending ++ bs ++ following.drop(bs.length)
-    } else if (idx <= underlying.length) {
-      // a new buffer partially fits into an old buffer
-      underlying.take(idx) ++ bs
-    } else {
-      // there is a gap (possibly empty) between an old buffer and a new buffer
-      underlying ++ zeros(idx - underlying.length) ++ bs
-    }
+
+    val newUnderlying: ByteString =
+      if (data.isEmpty)
+        underlying
+      else
+        underlying.take(idx).padTo(idx, 0.toByte) ++ data ++ underlying.drop(idx + data.length)
+
     new Memory(newUnderlying)
   }
 
@@ -57,11 +52,10 @@ class Memory(private[vm] val underlying: ByteString) {
 
   def load(offset: UInt256, size: UInt256): (ByteString, Memory) = doLoad(offset, size.toInt)
 
-  /** Returns a ByteString of a given size starting at the given address of the Memory.
-   * Underlying byte array is expanded and filled with 0's if addr + size exceeds size
-   * of the memory.
-   * This method may throw OOM.
-   */
+  /** Returns a ByteString of a given size starting at the given offset of the Memory.
+    * The memory is automatically expanded (with zeroes) when reading previously uninitialised regions,
+    * hence an OOM error may be thrown.
+    */
   private def doLoad(offset: UInt256, size: Int): (ByteString, Memory) = {
     val start: Int = offset.toInt
     val end: Int = start + size
