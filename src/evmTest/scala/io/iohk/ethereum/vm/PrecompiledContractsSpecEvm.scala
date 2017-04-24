a2ff7ec70dfada741da3982b45ed5d32d49af919
Adam Smolarek <adam.smolarek@iohk.io>
2017-04-24 11:13:10 +0200

    fix evm test?

--- src/evmTest/scala/io/iohk/ethereum/vm/PrecompiledContractsSpecEvm.scala
+++ src/evmTest/scala/io/iohk/ethereum/vm/PrecompiledContractsSpecEvm.scala
@@ -1,25 +1,26 @@
 package io.iohk.ethereum.vm
 
 import akka.util.ByteString
+import io.iohk.ethereum.crypto
 import io.iohk.ethereum.crypto._
-import io.iohk.ethereum.domain.{Address, SignedTransaction, Transaction}
+import io.iohk.ethereum.domain.SignedTransaction.{FirstByteOfAddress, LastByteOfAddress}
 import io.iohk.ethereum.vm.utils.EvmTestEnv
 import org.scalatest.{FunSuite, Matchers}
+import org.spongycastle.crypto.params.ECPublicKeyParameters
 
 class PrecompiledContractsSpecEvm extends FunSuite with Matchers {
 
   test("Precompiled Contracts") {
     val keyPair = generateKeyPair()
-    val tx = Transaction(42, 10, 1000, Address(123), 0, ByteString.empty)
-    val stx = SignedTransaction.sign(tx, keyPair, 0x3d.toByte)
-
-    val expectedOutput = ripemd160(kec256(stx.senderAddress.bytes))
+    val bytes: Array[Byte] = ByteString("aabbccdd").toArray[Byte]
+    val signature = ECDSASignature.sign(bytes, keyPair)
+    val pubKey = keyPair.getPublic.asInstanceOf[ECPublicKeyParameters].getQ.getEncoded(false)
+    val address = crypto.kec256(pubKey.tail).slice(FirstByteOfAddress, LastByteOfAddress)
+    val expectedOutput = ripemd160(kec256(address))
 
     new EvmTestEnv {
       val (_, contract) = deployContract("PrecompiledContracts")
-      //FIXME: stx should use BigInts?
-      val bytes = SignedTransaction.bytesToSign(tx, 0x3d.toByte)
-      val result = contract.usePrecompiledContracts(bytes, stx.signature.v, stx.signature.r, stx.signature.s).call()
+      val result = contract.usePrecompiledContracts(bytes, signature.v, signature.r, signature.s).call()
 
       // even though contract specifies bytes20 as the return type, 32-byte (right zero padded) value is returned
       result.returnData.take(20) shouldEqual expectedOutput
