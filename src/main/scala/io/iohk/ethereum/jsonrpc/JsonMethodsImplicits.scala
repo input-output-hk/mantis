596630bd39988f0478782b03dca6cf3fc13094cc
Lukasz Gasior <lukasz.gasior@iohk.io>
2017-05-09 12:10:14 +0200

    Add json utils for unformatted data and quantity

--- src/main/scala/io/iohk/ethereum/jsonrpc/JsonMethodsImplicits.scala
+++ src/main/scala/io/iohk/ethereum/jsonrpc/JsonMethodsImplicits.scala
@@ -19,23 +19,42 @@ object JsonMethodsImplicits {
   implicit val web3_sha3 = new JsonDecoder[Sha3Request] with JsonEncoder[Sha3Response] {
     override def decodeJson(params: Option[JArray]): Either[JsonRpcError, Sha3Request] =
       params match {
-        case Some(JArray((input: JString) :: Nil)) if input.s.startsWith("0x") =>
-          Try(ByteString(Hex.decode(input.s.drop(2)))) match {
-            case Success(bs) => Right(Sha3Request(bs))
-            case Failure(_) => Left(InvalidParams)
-          }
-
+        case Some(JArray((input: JString) :: Nil)) => tryExtractUnformattedData(input).map(Sha3Request)
         case _ => Left(InvalidParams)
       }
 
-    override def encodeJson(t: Sha3Response): JValue =
-      JString(s"0x${Hex.toHexString(t.data.toArray[Byte])}")
+    override def encodeJson(t: Sha3Response): JValue = encodeAsHex(t.data)
   }
 
   implicit val web3_clientVersion = new JsonDecoder[ClientVersionRequest] with JsonEncoder[ClientVersionResponse] {
     override def decodeJson(params: Option[JArray]): Either[JsonRpcError, ClientVersionRequest] = Right(ClientVersionRequest())
 
     override def encodeJson(t: ClientVersionResponse): JValue = t.value
+  }
+
+  private def encodeAsHex(input: ByteString): JString =
+    JString(s"0x${Hex.toHexString(input.toArray[Byte])}")
+
+  private def encodeAsHex(input: BigInt): JString =
+    JString(s"0x${Hex.toHexString(input.toByteArray)}")
+
+  private def tryExtractUnformattedData(input: JString): Either[JsonRpcError, ByteString] = {
+    if (input.s.startsWith("0x")) {
+      Try(ByteString(Hex.decode(input.s.drop(2)))) match {
+        case Success(bs) => Right(bs)
+        case Failure(_) => Left(InvalidParams.copy(message = s"Unable to parse data from '${input.s}'"))
+      }
+    } else Left(InvalidParams.copy(message = s"Data '${input.s}' should have 0x prefix"))
+  }
+
+  private def tryExtractQuantity(input: JString): Either[JsonRpcError, BigInt] = {
+    if (input.s.startsWith("0x")) {
+      val noPrefix = input.s.replace("0x", "")
+      Try(BigInt(noPrefix, 16)) match {
+        case Success(bi) => Right(bi)
+        case Failure(_) => Left(InvalidParams.copy(message = s"Unable to parse quantity from '${input.s}'"))
+      }
+    } else Left(InvalidParams.copy(message = s"Quantity '${input.s}' should have 0x prefix"))
   }
 
 }
