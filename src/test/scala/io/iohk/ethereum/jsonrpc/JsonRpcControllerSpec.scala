29452566115878797a09e20769872e888e99b19a
Lukasz Gasior <lukasz.gasior@iohk.io>
2017-05-09 11:46:20 +0200

    Add tests

---
+++ src/test/scala/io/iohk/ethereum/jsonrpc/JsonRpcControllerSpec.scala
@@ -0,0 +1,50 @@
+package io.iohk.ethereum.jsonrpc
+
+import io.circe.Json.JString
+import org.json4s.JsonAST._
+import org.json4s.JsonDSL._
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+class JsonRpcControllerSpec extends FlatSpec with Matchers {
+
+  "JsonRpcController" should "handle valid sha3 request" in new TestSetup {
+    val rpcRequest = JsonRpcRequest("2.0", "web3_sha3", Some(JArray(JString("0x1234") :: Nil)), Some(1))
+
+    val response = Await.result(jsonRpcController.handleRequest(rpcRequest), Duration.Inf)
+
+    response.jsonrpc shouldBe "2.0"
+    response.id shouldBe JInt(1)
+    response.error shouldBe None
+    response.result shouldBe Some(JString("0x56570de287d73cd1cb6092bb8fdee6173974955fdef345ae579ee9f475ea7432"))
+  }
+
+  it should "fail when invalid request is received" in new TestSetup {
+    val rpcRequest = JsonRpcRequest("2.0", "web3_sha3", Some(JArray(JString("asdasd") :: Nil)), Some(1))
+
+    val response = Await.result(jsonRpcController.handleRequest(rpcRequest), Duration.Inf)
+
+    response.jsonrpc shouldBe "2.0"
+    response.id shouldBe JInt(1)
+    response.error shouldBe Some(JsonRpcErrors.InvalidParams)
+  }
+
+  it should "handle clientVersion request" in new TestSetup {
+    val rpcRequest = JsonRpcRequest("2.0", "web3_clientVersion", None, Some(1))
+
+    val response = Await.result(jsonRpcController.handleRequest(rpcRequest), Duration.Inf)
+
+    response.jsonrpc shouldBe "2.0"
+    response.id shouldBe JInt(1)
+    response.error shouldBe None
+    response.result shouldBe Some(JString("etc-client/v0.1"))
+  }
+
+  trait TestSetup {
+    val web3Service = new Web3Service
+    val jsonRpcController = new JsonRpcController(web3Service)
+  }
+
+}
