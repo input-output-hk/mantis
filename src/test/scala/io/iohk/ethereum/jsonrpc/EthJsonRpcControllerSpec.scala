90c8c6fe7ff182c45a824e67840186d848ab11ec
Nicolas Tallar <nicolas.tallar@iohk.io>
2017-05-10 12:09:49 -0300

    Split tests into JsonRpcControllerSpec and EthJsonRpcControllerSpec

--- src/test/scala/io/iohk/ethereum/jsonrpc/EthJsonRpcControllerSpec.scala
+++ src/test/scala/io/iohk/ethereum/jsonrpc/EthJsonRpcControllerSpec.scala
@@ -3,65 +3,43 @@ package io.iohk.ethereum.jsonrpc
 import io.iohk.ethereum.Fixtures
 import io.iohk.ethereum.db.components.{SharedEphemDataSources, Storages}
 import io.iohk.ethereum.domain.{Block, BlockchainImpl}
-import io.iohk.ethereum.jsonrpc.JsonSerializers.{BlockResponseSerializer, QuantitiesSerializer}
+import io.iohk.ethereum.jsonrpc.EthService._
 import io.iohk.ethereum.network.p2p.messages.PV62.BlockBody
-import org.json4s.{DefaultFormats, Extraction}
-import org.json4s.JsonAST._
 import org.scalatest.{Matchers, WordSpec}
 import org.scalatest.prop.PropertyChecks
-import org.spongycastle.util.encoders.Hex
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-import scala.concurrent.Await
 import scala.concurrent.duration.Duration
+import scala.concurrent.Await
 
 class EthJsonRpcControllerSpec extends WordSpec with Matchers with PropertyChecks {
 
-  implicit val format = DefaultFormats + BlockResponseSerializer + QuantitiesSerializer
-
-  "JsonRpcController when sent eth_getBlockTransactionCountByHash" should {
+  "eth_getBlockTransactionCountByHash" should {
 
     val blockToRequest = Block(Fixtures.Blocks.Block3125369.header, Fixtures.Blocks.Block3125369.body)
     val blockToRequestHash = blockToRequest.header.hash
-    val request = JsonRpcRequest(
-      "2.0",
-      "eth_getBlockTransactionCountByHash",
-      Some(JArray(List(JString(s"0x${Hex.toHexString(blockToRequestHash.toArray)}")))),
-      Some(JInt(1))
-    )
+    val request = TxCountByBlockHashRequest(blockToRequestHash)
 
     "return None when the requested block isn't in the blockchain" in new TestSetup {
-      val response = Await.result(jsonRpcController.handleRequest(request), Duration.Inf)
-      response.jsonrpc shouldBe "2.0"
-      response.id shouldBe JInt(1)
-      response.error shouldBe None
-      response.result shouldBe Some(JNull)
+      val response = Await.result(ethService.getBlockTransactionCountByHash(request), Duration.Inf)
+      response.txsQuantity shouldBe None
     }
 
     "return that the block has no tx when the requested block is in the blockchain and has no tx" in new TestSetup {
       blockchain.save(blockToRequest.copy(body = BlockBody(Nil, Nil)))
-      val response = Await.result(jsonRpcController.handleRequest(request), Duration.Inf)
-      response.jsonrpc shouldBe "2.0"
-      response.id shouldBe JInt(1)
-      response.error shouldBe None
-      response.result shouldBe Some(JString("0x0"))
+      val response = Await.result(ethService.getBlockTransactionCountByHash(request), Duration.Inf)
+      response.txsQuantity shouldBe Some(0)
     }
 
     "return the correct number of txs when the requested block is in the blockchain and has some tx" in new TestSetup {
       blockchain.save(blockToRequest)
-      val response = Await.result(jsonRpcController.handleRequest(request), Duration.Inf)
-      val expectedTxCount = Extraction.decompose(BigInt(blockToRequest.body.transactionList.size))
-
-      response.jsonrpc shouldBe "2.0"
-      response.id shouldBe JInt(1)
-      response.error shouldBe None
-      response.result shouldBe Some(expectedTxCount)
+      val response = Await.result(ethService.getBlockTransactionCountByHash(request), Duration.Inf)
+      response.txsQuantity shouldBe Some(blockToRequest.body.transactionList.size)
     }
 
   }
 
-  "JsonRpcController when sent eth_getBlockByHash" should {
+  "eth_getBlockByHash" should {
 
     val blockToRequest = Block(Fixtures.Blocks.Block3125369.header, Fixtures.Blocks.Block3125369.body)
     val blockToRequestHash = blockToRequest.header.hash
@@ -70,149 +48,99 @@ class EthJsonRpcControllerSpec extends WordSpec with Matchers with PropertyCheck
       TransactionResponse(stx, Some(blockToRequest.header), Some(txIndex))
     }
 
-    val request = JsonRpcRequest(
-      "2.0",
-      "eth_getBlockByHash",
-      Some(JArray(List(JString(s"0x${Hex.toHexString(blockToRequestHash.toArray)}"), JBool(true)))),
-      Some(JInt(1))
-    )
+    val request = BlockByBlockHashRequest(blockToRequestHash, fullTxs = true)
 
     "return None when the requested block isn't in the blockchain" in new TestSetup {
-      val response = Await.result(jsonRpcController.handleRequest(request), Duration.Inf)
-
-      response.jsonrpc shouldBe "2.0"
-      response.id shouldBe JInt(1)
-      response.error shouldBe None
-      response.result shouldBe Some(JNull)
+      val response = Await.result(ethService.getByBlockHash(request), Duration.Inf)
+      response.blockView shouldBe None
     }
 
     "return the block view correctly when it's totalDifficulty is in blockchain" in new TestSetup {
       blockchain.save(blockToRequest)
       blockchain.save(blockToRequestHash, blockTd)
-      val response = Await.result(jsonRpcController.handleRequest(request), Duration.Inf)
+      val response = Await.result(ethService.getByBlockHash(request), Duration.Inf)
 
-      val expectedBlockView = Extraction.decompose(BlockResponse(blockToRequest, fullTxs = true, totalDifficulty = Some(blockTd)))
-
-      response.jsonrpc shouldBe "2.0"
-      response.id shouldBe JInt(1)
-      response.error shouldBe None
-      response.result shouldBe Some(expectedBlockView)
+      response.blockView shouldBe Some(BlockResponse(blockToRequest, fullTxs = true, totalDifficulty = Some(blockTd)))
+      response.blockView.get.totalDifficulty shouldBe Some(blockTd)
+      response.blockView.get.transactions.right.toOption shouldBe Some(stxsViews)
     }
 
     "return the block view correctly when it's totalDifficulty is not in blockchain" in new TestSetup {
       blockchain.save(blockToRequest)
-      val response = Await.result(jsonRpcController.handleRequest(request), Duration.Inf)
+      val response = Await.result(ethService.getByBlockHash(request), Duration.Inf)
 
-      val expectedBlockView = Extraction.decompose(BlockResponse(blockToRequest, fullTxs = true, totalDifficulty = None))
-
-      response.jsonrpc shouldBe "2.0"
-      response.id shouldBe JInt(1)
-      response.error shouldBe None
-      response.result shouldBe Some(expectedBlockView)
+      response.blockView shouldBe Some(BlockResponse(blockToRequest, fullTxs = true))
+      response.blockView.get.totalDifficulty shouldBe None
+      response.blockView.get.transactions.right.toOption shouldBe Some(stxsViews)
     }
 
     "return the block view correctly when the txs should be hashed" in new TestSetup {
       blockchain.save(blockToRequest)
       blockchain.save(blockToRequestHash, blockTd)
-      val paramsWithTxHashedTrue = Some(JArray(List(JString(s"0x${Hex.toHexString(blockToRequestHash.toArray)}"), JBool(false))))
-      val response = Await.result(jsonRpcController.handleRequest(request.copy(params = paramsWithTxHashedTrue)), Duration.Inf)
+      val response = Await.result(ethService.getByBlockHash(request.copy(fullTxs = false)), Duration.Inf)
 
-      val expectedBlockView = Extraction.decompose(BlockResponse(blockToRequest, fullTxs = false, totalDifficulty = Some(blockTd)))
-
-      response.jsonrpc shouldBe "2.0"
-      response.id shouldBe JInt(1)
-      response.error shouldBe None
-      response.result shouldBe Some(expectedBlockView)
+      response.blockView shouldBe Some(BlockResponse(blockToRequest, fullTxs = false, totalDifficulty = Some(blockTd)))
+      response.blockView.get.totalDifficulty shouldBe Some(blockTd)
+      response.blockView.get.transactions.left.toOption shouldBe Some(blockToRequest.body.transactionList.map(_.hash))
     }
 
   }
 
 
-  "JsonRpcController when sent eth_getUncleByBlockHashAndIndex" should {
+  "eth_getUncleByBlockHashAndIndex" should {
 
     val blockToRequest = Block(Fixtures.Blocks.Block3125369.header, Fixtures.Blocks.Block3125369.body)
     val blockToRequestHash = blockToRequest.header.hash
     val uncleIndexToRequest = 0
+    val request = UncleByBlockHashAndIndexRequest(blockToRequestHash, uncleIndexToRequest)
 
     val uncle = Fixtures.Blocks.DaoForkBlock.header
     val uncleTd = uncle.difficulty
     val blockToRequestWithUncles = blockToRequest.copy(body = BlockBody(Nil, Seq(uncle)))
     val blockToRequestWithUnclesHash = blockToRequestWithUncles.header.hash
-
-    def requestForIndex(uncleIndex: Int): JsonRpcRequest = JsonRpcRequest(
-      "2.0",
-      "eth_getUncleByBlockHashAndIndex",
-      Some(JArray(List(JString(s"0x${Hex.toHexString(blockToRequestHash.toArray)}"), JString(s"0x${Hex.toHexString(BigInt(uncleIndex).toByteArray)}")))),
-      Some(JInt(1))
-    )
+    val requestForBlockWithUncles = request.copy(blockToRequestWithUnclesHash)
 
     "return None when the requested block isn't in the blockchain" in new TestSetup {
-      val response = Await.result(jsonRpcController.handleRequest(requestForIndex(uncleIndexToRequest)), Duration.Inf)
-
-      response.jsonrpc shouldBe "2.0"
-      response.id shouldBe JInt(1)
-      response.error shouldBe None
-      response.result shouldBe Some(JNull)
+      val response = Await.result(ethService.getUncleByBlockHashAndIndex(request), Duration.Inf)
+      response.uncleBlockView shouldBe None
     }
 
     "return None when there's no uncle" in new TestSetup {
       blockchain.save(blockToRequest)
-      val response = Await.result(jsonRpcController.handleRequest(requestForIndex(uncleIndexToRequest)), Duration.Inf)
+      val response = Await.result(ethService.getUncleByBlockHashAndIndex(request), Duration.Inf)
 
-      response.jsonrpc shouldBe "2.0"
-      response.id shouldBe JInt(1)
-      response.error shouldBe None
-      response.result shouldBe Some(JNull)
+      response.uncleBlockView shouldBe None
     }
 
     "return None when there's no uncle in the requested index" in new TestSetup {
       blockchain.save(blockToRequestWithUncles)
 
-      val response1 = Await.result(jsonRpcController.handleRequest(requestForIndex(1)), Duration.Inf)
-      response1.jsonrpc shouldBe "2.0"
-      response1.id shouldBe JInt(1)
-      response1.error shouldBe None
-      response1.result shouldBe Some(JNull)
+      val response1 = Await.result(ethService.getUncleByBlockHashAndIndex(request.copy(uncleIndex = 1)), Duration.Inf)
+      val response2 = Await.result(ethService.getUncleByBlockHashAndIndex(request.copy(uncleIndex = -1)), Duration.Inf)
 
-      val response2 = Await.result(jsonRpcController.handleRequest(requestForIndex(-1)), Duration.Inf)
-      response2.jsonrpc shouldBe "2.0"
-      response2.id shouldBe JInt(1)
-      response2.error shouldBe None
-      response2.result shouldBe Some(JNull)
-
+      response1.uncleBlockView shouldBe None
+      response2.uncleBlockView shouldBe None
     }
 
     "return the uncle block view correctly when the requested index has one but there's no total difficulty for it" in new TestSetup {
       blockchain.save(blockToRequestWithUncles)
-      val response = Await.result(jsonRpcController.handleRequest(requestForIndex(uncleIndexToRequest)), Duration.Inf)
+      val response = Await.result(ethService.getUncleByBlockHashAndIndex(request), Duration.Inf)
 
-      val expectedUncleBlockView = Extraction.decompose(BlockResponse(uncle, None))
-        .removeField{
-          case ("transactions", _) => true
-          case _ => false
-        }
-
-      response.jsonrpc shouldBe "2.0"
-      response.id shouldBe JInt(1)
-      response.error shouldBe None
-      response.result shouldBe Some(expectedUncleBlockView)
+      response.uncleBlockView shouldBe Some(BlockResponse(uncle, None))
+      response.uncleBlockView.get.totalDifficulty shouldBe None
+      response.uncleBlockView.get.transactions shouldBe Left(Nil)
+      response.uncleBlockView.get.uncles shouldBe Nil
     }
 
     "return the uncle block view correctly when the requested index has one and there's total difficulty for it" in new TestSetup {
       blockchain.save(blockToRequestWithUncles)
       blockchain.save(uncle.hash, uncleTd)
-      val response = Await.result(jsonRpcController.handleRequest(requestForIndex(uncleIndexToRequest)), Duration.Inf)
+      val response = Await.result(ethService.getUncleByBlockHashAndIndex(request), Duration.Inf)
 
-      val expectedUncleBlockView = Extraction.decompose(BlockResponse(uncle, Some(uncleTd)))
-        .removeField{
-          case ("transactions", _) => true
-          case _ => false
-        }
-
-      response.jsonrpc shouldBe "2.0"
-      response.id shouldBe JInt(1)
-      response.error shouldBe None
-      response.result shouldBe Some(expectedUncleBlockView)
+      response.uncleBlockView shouldBe Some(BlockResponse(uncle, Some(uncleTd)))
+      response.uncleBlockView.get.totalDifficulty shouldBe Some(uncleTd)
+      response.uncleBlockView.get.transactions shouldBe Left(Nil)
+      response.uncleBlockView.get.uncles shouldBe Nil
     }
   }
 
@@ -220,9 +148,7 @@ class EthJsonRpcControllerSpec extends WordSpec with Matchers with PropertyCheck
     val storagesInstance = new SharedEphemDataSources with Storages.DefaultStorages
     val blockchain = BlockchainImpl(storagesInstance.storages)
 
-    val web3Service = new Web3Service
     val ethService = new EthService(blockchain)
-    val jsonRpcController = new JsonRpcController(web3Service, ethService)
   }
 
 }
--- src/test/scala/io/iohk/ethereum/jsonrpc/JsonRpcControllerSpec.scala
+++ src/test/scala/io/iohk/ethereum/jsonrpc/JsonRpcControllerSpec.scala
@@ -1,11 +1,15 @@
 package io.iohk.ethereum.jsonrpc
 
-import io.circe.Json.JString
+import io.iohk.ethereum.Fixtures
 import io.iohk.ethereum.db.components.{SharedEphemDataSources, Storages}
-import io.iohk.ethereum.domain.BlockchainImpl
+import io.iohk.ethereum.domain.{Block, BlockchainImpl}
+import io.iohk.ethereum.jsonrpc.JsonSerializers.{BlockResponseSerializer, QuantitiesSerializer}
+import io.iohk.ethereum.network.p2p.messages.PV62.BlockBody
+import org.json4s.{DefaultFormats, Extraction}
 import org.json4s.JsonAST._
 import org.json4s.JsonDSL._
 import org.scalatest.{FlatSpec, Matchers}
+import org.spongycastle.util.encoders.Hex
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
@@ -42,6 +46,85 @@ class JsonRpcControllerSpec extends FlatSpec with Matchers {
     response.id shouldBe JInt(1)
     response.error shouldBe None
     response.result shouldBe Some(JString("etc-client/v0.1"))
+  }
+
+  it should "handle eth_getBlockTransactionCountByHash request" in new TestSetup {
+    implicit val format = DefaultFormats + BlockResponseSerializer + QuantitiesSerializer
+
+    val blockToRequest = Block(Fixtures.Blocks.Block3125369.header, Fixtures.Blocks.Block3125369.body)
+
+    blockchain.save(blockToRequest)
+
+    val rpcRequest = JsonRpcRequest(
+      "2.0",
+      "eth_getBlockTransactionCountByHash",
+      Some(JArray(List(JString(s"0x${blockToRequest.header.hashAsHexString}")))),
+      Some(JInt(1))
+    )
+    val response = Await.result(jsonRpcController.handleRequest(rpcRequest), Duration.Inf)
+
+    val expectedTxCount = Extraction.decompose(BigInt(blockToRequest.body.transactionList.size))
+
+    response.jsonrpc shouldBe "2.0"
+    response.id shouldBe JInt(1)
+    response.error shouldBe None
+    response.result shouldBe Some(expectedTxCount)
+  }
+
+  it should "handle eth_getBlockByHash request" in new TestSetup {
+    implicit val format = DefaultFormats + BlockResponseSerializer + QuantitiesSerializer
+
+    val blockToRequest = Block(Fixtures.Blocks.Block3125369.header, Fixtures.Blocks.Block3125369.body)
+    val blockTd = blockToRequest.header.difficulty
+
+    blockchain.save(blockToRequest)
+    blockchain.save(blockToRequest.header.hash, blockTd)
+
+    val request = JsonRpcRequest(
+      "2.0",
+      "eth_getBlockByHash",
+      Some(JArray(List(JString(s"0x${blockToRequest.header.hashAsHexString}"), JBool(false)))),
+      Some(JInt(1))
+    )
+    val response = Await.result(jsonRpcController.handleRequest(request), Duration.Inf)
+
+    val expectedBlockView = Extraction.decompose(BlockResponse(blockToRequest, fullTxs = false, totalDifficulty = Some(blockTd)))
+
+    response.jsonrpc shouldBe "2.0"
+    response.id shouldBe JInt(1)
+    response.error shouldBe None
+    response.result shouldBe Some(expectedBlockView)
+  }
+
+  it should "handle eth_getUncleByBlockHashAndIndex request" in new TestSetup {
+    implicit val format = DefaultFormats + BlockResponseSerializer + QuantitiesSerializer
+
+    val uncle = Fixtures.Blocks.DaoForkBlock.header
+    val blockToRequest = Block(Fixtures.Blocks.Block3125369.header, BlockBody(Nil, Seq(uncle)))
+
+    blockchain.save(blockToRequest)
+
+    val request: JsonRpcRequest = JsonRpcRequest(
+      "2.0",
+      "eth_getUncleByBlockHashAndIndex",
+      Some(JArray(List(
+        JString(s"0x${blockToRequest.header.hashAsHexString}"),
+        JString(s"0x${Hex.toHexString(BigInt(0).toByteArray)}")
+      ))),
+      Some(JInt(1))
+    )
+    val response = Await.result(jsonRpcController.handleRequest(request), Duration.Inf)
+
+    val expectedUncleBlockView = Extraction.decompose(BlockResponse(uncle, None))
+      .removeField{
+        case ("transactions", _) => true
+        case _ => false
+      }
+
+    response.jsonrpc shouldBe "2.0"
+    response.id shouldBe JInt(1)
+    response.error shouldBe None
+    response.result shouldBe Some(expectedUncleBlockView)
   }
 
   trait TestSetup {
