77300c65552813dabf407c0ba219383b35a5f3da
Radek Tkaczyk <radek.tkaczyk@iohk.io>
2017-06-27 12:17:53 +0200

    SecureRandom small fixes

--- src/evmTest/scala/io/iohk/ethereum/vm/PrecompiledContractsSpecEvm.scala
+++ src/evmTest/scala/io/iohk/ethereum/vm/PrecompiledContractsSpecEvm.scala
@@ -1,17 +1,17 @@
 package io.iohk.ethereum.vm
 
 import akka.util.ByteString
-import io.iohk.ethereum.crypto
+import io.iohk.ethereum.{SecureRandomProvider, crypto}
 import io.iohk.ethereum.crypto._
 import io.iohk.ethereum.domain.SignedTransaction.{FirstByteOfAddress, LastByteOfAddress}
 import io.iohk.ethereum.vm.utils.EvmTestEnv
 import org.scalatest.{FunSuite, Matchers}
 import org.spongycastle.crypto.params.ECPublicKeyParameters
 
-class PrecompiledContractsSpecEvm extends FunSuite with Matchers {
+class PrecompiledContractsSpecEvm extends FunSuite with Matchers with SecureRandomProvider {
 
   test("Precompiled Contracts") {
-    val keyPair = generateKeyPair()
+    val keyPair = generateKeyPair(secureRandom)
     val bytes: Array[Byte] = ByteString("aabbccdd").toArray[Byte]
     val signature = ECDSASignature.sign(bytes, keyPair)
     val pubKey = keyPair.getPublic.asInstanceOf[ECPublicKeyParameters].getQ.getEncoded(false)
--- src/test/scala/io/iohk/ethereum/SecureRandomProvider.scala
+++ src/test/scala/io/iohk/ethereum/SecureRandomProvider.scala
@@ -7,5 +7,3 @@ import io.iohk.ethereum.utils.Config
 trait SecureRandomProvider {
   val secureRandom: SecureRandom = SecureRandom.getInstance(Config.secureRandomAlgo)
 }
-
-object SecureRandomProvider extends SecureRandomProvider
