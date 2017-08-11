4fa9bf32a23f93f35eae6353e2e806ce540a480d
Nicolas Tallar <nicolas.tallar@iohk.io>
2017-08-11 12:19:00 -0300

    [EC-167] Added persistStateSnapshotDelay constant

--- src/main/scala/io/iohk/ethereum/blockchain/sync/FastSync.scala
+++ src/main/scala/io/iohk/ethereum/blockchain/sync/FastSync.scala
@@ -176,7 +176,10 @@ trait FastSync {
 
     private var blockChainOnlyPeers = Seq.empty[Peer]
 
-    private val syncStatePersistCancellable = scheduler.schedule(0.seconds, persistStateSnapshotInterval, self, PersistSyncState)
+    //Delay before starting to persist snapshot. It should be 0, as the presence of it marks that fast sync was started
+    private val persistStateSnapshotDelay: FiniteDuration = 0.seconds
+
+    private val syncStatePersistCancellable = scheduler.schedule(persistStateSnapshotDelay, persistStateSnapshotInterval, self, PersistSyncState)
     private val heartBeat = scheduler.schedule(syncRetryInterval, syncRetryInterval * 2, self, ProcessSyncing)
 
     // scalastyle:off cyclomatic.complexity
