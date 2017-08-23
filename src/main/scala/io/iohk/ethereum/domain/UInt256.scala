dd5cdd2e50440d3ea82f738ebc2a03968885f087
Adam Smolarek <adam.smolarek@iohk.io>
2017-08-23 14:42:19 +0200

    remove todo

--- src/main/scala/io/iohk/ethereum/domain/UInt256.scala
+++ src/main/scala/io/iohk/ethereum/domain/UInt256.scala
@@ -62,7 +62,6 @@ object UInt256 {
   private val MaxSignedValue: BigInt = BigInt(2).pow(Size * 8 - 1) - 1
 }
 
-// TODO: consider moving to util as Uint256, which follows Scala numeric conventions, and is used across the system for P_256 numbers (see YP 4.3) [EC-252]
 /** Represents 256 bit unsigned integers with standard arithmetic, byte-wise operation and EVM-specific extensions */
 class UInt256 private (private val n: BigInt) extends Ordered[UInt256] {
 
