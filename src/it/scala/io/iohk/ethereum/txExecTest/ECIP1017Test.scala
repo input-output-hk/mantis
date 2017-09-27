717ffe07697c24945a08894e2f7a18856bb4c634
KonradStaniec <konrad.staniec@gmail.com>
2017-09-27 13:48:57 +0200

    Revert test update and fix post merge issue

--- src/it/scala/io/iohk/ethereum/txExecTest/ECIP1017Test.scala
+++ src/it/scala/io/iohk/ethereum/txExecTest/ECIP1017Test.scala
@@ -30,6 +30,7 @@ class ECIP1017Test extends FlatSpec with Matchers {
     override val difficultyBombPauseBlockNumber: BigInt = Long.MaxValue
     override val difficultyBombContinueBlockNumber: BigInt = Long.MaxValue
     override val accountStartNonce: UInt256 = UInt256.Zero
+    override val eip161BlockNumber: BigInt = Long.MaxValue
   }
 
   val noErrors = a[Right[_, Seq[Receipt]]]
