72e8d7fa0f77ff37d40f595cbca9e9a5c91d53d5
Radek Tkaczyk <radek.tkaczyk@iohk.io>
2018-01-03 08:27:08 +0100

    adjusting JVM params for ETS only

--- test-ets.sh
+++ test-ets.sh
@@ -3,8 +3,15 @@
 # to differentiate by branch, use CIRCLE_BRANCH variable in the conditional below
 
 echo "current branch: $CIRCLE_BRANCH"
+
 echo "memory:"
 free -m
+
+# at least 1.5G of heap space and 10M of stack size is required for ETS.
+# however setting these JVM params globally for the whole build causes failures
+# due to CircleCI memory limit exceeded.
+export JAVA_OPTS="-Xmx2g -Xss16m -XX:MaxMetaspaceSize=512m"
+
 if [ -z $SKIP_ETS_TESTS ]; then
     git submodule init;
     git submodule update;
