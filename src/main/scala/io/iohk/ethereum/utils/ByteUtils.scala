de1ec72f90fd5ece46699a9ac7f63e0e79e23683
KonradStaniec <konrad.staniec@gmail.com>
2018-01-15 12:29:35 +0100

    Fix messages in DiscoveryManager
    Change nodeId to nodeAddress in ping endpoint
    Little refactor and change to built in function

--- src/main/scala/io/iohk/ethereum/utils/ByteUtils.scala
+++ src/main/scala/io/iohk/ethereum/utils/ByteUtils.scala
@@ -66,46 +66,6 @@ object ByteUtils {
     ByteString(data)
   }
 
-  val IPv4AddressLength = 4
-  val IPv6AddressLength = 16
-
-  /**
-    * Obtains the IP address from a 4-byte array (IPv4) or 16-byte array (IPv6)
-    *
-    * @param bytesIp
-    * @return the corresponding IP address
-    */
-  def bytesToIp(bytesIp: ByteString): Option[String] = {
-    bytesIp.length match {
-      case IPv4AddressLength => Some(bytesToIPv4(bytesIp))
-      case IPv6AddressLength => Some(bytesToIPv6(bytesIp))
-      case _ => None
-    }
-  }
-
-  private def bytesToIPv4(bytesIp: ByteString): String = {
-    val sb = new StringBuilder()
-    sb.append(bytesIp(0) & 0xFF)
-    sb.append(".")
-    sb.append(bytesIp(1) & 0xFF)
-    sb.append(".")
-    sb.append(bytesIp(2) & 0xFF)
-    sb.append(".")
-    sb.append(bytesIp(3) & 0xFF)
-    sb.toString()
-  }
-
-  private def bytesToIPv6(bytesIp: ByteString): String = {
-    val sb = new StringBuilder()
-    sb.append("[")
-    sb.append(bytesIp(0) & 0xFF)
-    (1 until IPv6AddressLength).foreach { i =>
-      sb.append(":")
-      sb.append(bytesIp(i) & 0xFF)
-    }
-    sb.append("]")
-    sb.toString()
-  }
 
   def bytesToInts(bytes: Array[Byte]): Array[Int] =
     bytes.grouped(4).map(getIntFromWord).toArray
