0afd0a57e9985f6fdde66720b3885d3be5f2a8ef
Christos KK Loverdos <christos.loverdos@iohk.io>
2018-04-25 23:41:25 +0300

    [CGKIELE-166] More accurate image tagging

--- docker/build.sh
+++ docker/build.sh
@@ -3,7 +3,18 @@
 set -eux
 
 HERE=$(readlink -m $(dirname ${BASH_SOURCE[0]}))
-DATE_TAG=$(date --iso-8601)
-TAG=${1:-$DATE_TAG}
 
-docker build -t mantis:$TAG $HERE
+IMAGE_NAME=mantis
+DOCKERFILE=Dockerfile
+
+# If IMAGE_TAG is not given in the command line, then compute one in the format
+#
+#   YYYY-MM-DD '.' SHORT_HASH
+#
+# where the variable parts refer to the latest commit
+IMAGE_TAG=${1:-$(git log -1 --format=%cd.%h --date=short)}
+
+# This is the commit that the image will be based on
+COMMIT=$(git log -1 --format=%H)
+
+docker build --build-arg MANTIS_TAG=${COMMIT} -t $IMAGE_NAME:$IMAGE_TAG -f $HERE/$DOCKERFILE $HERE
