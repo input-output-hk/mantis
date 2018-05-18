f204e6a0f87e0735693529a6138a0396c57cf575
Łukasz Gąsior <lukasz.gasior@iohk.io>
2018-05-18 13:52:12 +0200

    Specify keyfile to load wallet for faucet

--- src/main/scala/io/iohk/ethereum/faucet/FaucetConfig.scala
+++ src/main/scala/io/iohk/ethereum/faucet/FaucetConfig.scala
@@ -2,20 +2,19 @@ package io.iohk.ethereum.faucet
 
 import akka.http.scaladsl.model.headers.HttpOriginRange
 import com.typesafe.config.{Config => TypesafeConfig}
-import io.iohk.ethereum.domain.Address
 import io.iohk.ethereum.utils.ConfigUtils
 
 import scala.concurrent.duration.{FiniteDuration, _}
 
 case class FaucetConfig(
-    walletAddress: Address,
+    keyfile: String,
+    keyStoreDir: String,
     walletPassword: String,
     txGasPrice: BigInt,
     txGasLimit: BigInt,
     txValue: BigInt,
     corsAllowedOrigins: HttpOriginRange,
     rpcAddress: String,
-    keyStoreDir: String,
     listenInterface: String,
     listenPort: Int,
     minRequestInterval: FiniteDuration,
@@ -28,14 +27,14 @@ object FaucetConfig {
     val corsAllowedOrigins = ConfigUtils.parseCorsAllowedOrigins(faucetConfig, "cors-allowed-origins")
 
     FaucetConfig(
-      walletAddress = Address(faucetConfig.getString("wallet-address")),
+      keyfile = faucetConfig.getString("keyfile"),
+      keyStoreDir = faucetConfig.getString("keystore-dir"),
       walletPassword = faucetConfig.getString("wallet-password"),
       txGasPrice = faucetConfig.getLong("tx-gas-price"),
       txGasLimit = faucetConfig.getLong("tx-gas-limit"),
       txValue = faucetConfig.getLong("tx-value"),
       corsAllowedOrigins = corsAllowedOrigins,
       rpcAddress = faucetConfig.getString("rpc-address"),
-      keyStoreDir = faucetConfig.getString("keystore-dir"),
       listenInterface = faucetConfig.getString("listen-interface"),
       listenPort = faucetConfig.getInt("listen-port"),
       minRequestInterval = faucetConfig.getDuration("min-request-interval").toMillis.millis,
