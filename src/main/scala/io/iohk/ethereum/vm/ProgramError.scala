24e95f9315d564924be684e233f263894045bf79
Michal Mrozek <michal.mrozek@iohk.io>
2018-06-18 17:07:00 +0200

    EIP-214 - STATICCALL opcode

--- src/main/scala/io/iohk/ethereum/vm/ProgramError.scala
+++ src/main/scala/io/iohk/ethereum/vm/ProgramError.scala
@@ -12,6 +12,10 @@ case class  InvalidOpCode(code: Byte) extends ProgramError {
   override def toString: String =
     f"${getClass.getSimpleName}(0x${code.toInt & 0xff}%02x)"
 }
+case class  OpCodeNotAvailableInStaticContext(code: Byte) extends ProgramError {
+  override def toString: String =
+    f"${getClass.getSimpleName}(0x${code.toInt & 0xff}%02x)"
+}
 case object OutOfGas extends ProgramError
 case class InvalidJump(dest: UInt256) extends ProgramError {
   override def toString: String =
