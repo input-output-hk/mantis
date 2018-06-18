1343c59940576a89938feabd6b0895b1815e0998
Kris Jenkins <krisajenkins@gmail.com>
2018-06-18 13:26:54 +0100

    Hydra: Pinning nixpkgs to 18.03.

--- jobsets/default.nix
+++ jobsets/default.nix
@@ -26,7 +26,7 @@ let pkgs = import nixpkgs {};
 
           nixpkgs = mkGitSrc {
             repo = "https://github.com/NixOS/nixpkgs.git";
-            branch = "06c576b0525da85f2de86b3c13bb796d6a0c20f6";
+            branch = "refs/tags/18.03";
           };
           sbtVerifySrc = mkGitSrc {
             repo = "https://github.com/input-output-hk/sbt-verify.git";
