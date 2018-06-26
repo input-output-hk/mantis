4da9260ebcd2ece965da44b90fae73d17abff580
Michal Mrozek <michal.mrozek@iohk.io>
2018-06-26 15:04:35 +0200

    merge from byzantium fork

--- src/main/scala/io/iohk/ethereum/utils/Config.scala
+++ src/main/scala/io/iohk/ethereum/utils/Config.scala
@@ -310,7 +310,6 @@ trait BlockchainConfig {
   val eip155BlockNumber: BigInt
   val eip160BlockNumber: BigInt
   val eip161BlockNumber: BigInt
-  val byzantiumBlockNumber: BigInt
   val maxCodeSize: Option[BigInt]
   val difficultyBombPauseBlockNumber: BigInt
   val difficultyBombContinueBlockNumber: BigInt
@@ -346,7 +345,6 @@ object BlockchainConfig {
       override val eip155BlockNumber: BigInt = BigInt(blockchainConfig.getString("eip155-block-number"))
       override val eip160BlockNumber: BigInt = BigInt(blockchainConfig.getString("eip160-block-number"))
       override val eip161BlockNumber: BigInt = BigInt(blockchainConfig.getString("eip161-block-number"))
-      override val byzantiumBlockNumber: BigInt = BigInt(blockchainConfig.getString("byzantium-block-number"))
       override val maxCodeSize: Option[BigInt] = Try(BigInt(blockchainConfig.getString("max-code-size"))).toOption
       override val difficultyBombPauseBlockNumber: BigInt = BigInt(blockchainConfig.getString("difficulty-bomb-pause-block-number"))
       override val difficultyBombContinueBlockNumber: BigInt = BigInt(blockchainConfig.getString("difficulty-bomb-continue-block-number"))
--- src/test/scala/io/iohk/ethereum/consensus/validators/BlockHeaderValidatorSpec.scala
+++ src/test/scala/io/iohk/ethereum/consensus/validators/BlockHeaderValidatorSpec.scala
@@ -296,7 +296,6 @@ class BlockHeaderValidatorSpec extends FlatSpec with Matchers with PropertyCheck
       override val eip160BlockNumber: BigInt = Long.MaxValue
       override val eip161BlockNumber: BigInt = Long.MaxValue
       override val eip150BlockNumber: BigInt = Long.MaxValue
-      override val byzantiumBlockNumber: BigInt = Long.MaxValue
       override val eip106BlockNumber: BigInt = 0
       override val byzantiumBlockNumber: BigInt = Long.MaxValue
       override val chainId: Byte = 0x3d.toByte
