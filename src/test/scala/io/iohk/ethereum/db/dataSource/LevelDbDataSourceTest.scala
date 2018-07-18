b8906d12e2cb96bf7578e9965237540d442d50df
Agnieszka Kowal <agnieszka.kowal@scalac.io>
2018-07-18 15:22:51 +0200

    Rearrange leveldb data source test, change fork config to work in all tests

--- src/test/scala/io/iohk/ethereum/db/dataSource/LevelDbDataSourceTest.scala
+++ src/test/scala/io/iohk/ethereum/db/dataSource/LevelDbDataSourceTest.scala
@@ -14,11 +14,12 @@ class LevelDbDataSourceTest extends FlatSpec with DataSourceTestBehavior {
       override val paranoidChecks: Boolean = true
       override val createIfMissing: Boolean = true
       override val path: String = dbPath
-      override val native: Boolean = false
+      override val native: Boolean = true
       override val maxOpenFiles: Int = 32
     })
   }
 
   it should behave like dataSource(createDataSource)
+
 }
 
