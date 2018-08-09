82f7707f16fa42972224853413dcdcb4ea522141
Michal Mrozek <michal.mrozek@iohk.io>
2018-08-09 13:52:51 +0200

    merge from release2.0

--- src/test/scala/io/iohk/ethereum/db/dataSource/LevelDbDataSourceTest.scala
+++ src/test/scala/io/iohk/ethereum/db/dataSource/LevelDbDataSourceTest.scala
@@ -1,22 +1,15 @@
 package io.iohk.ethereum.db.dataSource
 
-import java.nio.file.Files
-
 import org.scalatest.FlatSpec
 
-class LevelDbDataSourceTest extends FlatSpec with DataSourceTestBehavior {
+class LevelDBDataSourceTest extends FlatSpec with DataSourceTestBehavior {
 
-  private def createDataSource(path: String): LevelDBDataSource = {
-    val dbPath: String = Files.createTempDirectory("temp-test-leveldb").toAbsolutePath.toString
-
-    LevelDBDataSource(new LevelDbConfig {
-      override val verifyChecksums: Boolean = true
-      override val paranoidChecks: Boolean = true
-      override val createIfMissing: Boolean = true
-      override val path: String = dbPath
-    })
-  }
+  private def createDataSource(path: String) = LevelDBDataSource(new LevelDbConfig {
+    override val verifyChecksums: Boolean = true
+    override val paranoidChecks: Boolean = true
+    override val createIfMissing: Boolean = true
+    override val path: String = "/tmp/test/leveldb/"
+  })
 
   it should behave like dataSource(createDataSource)
-
 }
