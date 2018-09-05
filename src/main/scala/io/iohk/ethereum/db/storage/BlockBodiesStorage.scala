0915172cc25d9d03cbd8ad58d65c47a54abe269d
Agnieszka Kowal <agnieszka.kowal@scalac.io>
2018-09-05 16:36:35 +0200

    Add tests for mapping

--- src/main/scala/io/iohk/ethereum/db/storage/BlockBodiesStorage.scala
+++ src/main/scala/io/iohk/ethereum/db/storage/BlockBodiesStorage.scala
@@ -21,14 +21,12 @@ class BlockBodiesStorage(val dataSource: DataSource) extends KeyValueStorage[Blo
 
   override val namespace: IndexedSeq[Byte] = Namespaces.BodyNamespace
 
+  override def keySerializer: BlockBodyHash => IndexedSeq[Byte] = _.toIndexedSeq
 
-  override def keySerializer: BlockBodyHash => IndexedSeq[Byte] = identity
-
-  override def valueSerializer: BlockBody => IndexedSeq[Byte] = tl => compactPickledBytes(Pickle.intoBytes(tl))
+  override def valueSerializer: BlockBody => IndexedSeq[Byte] = blockBody => compactPickledBytes(Pickle.intoBytes(blockBody))
 
   override def valueDeserializer: IndexedSeq[Byte] => BlockBody =
-    b => Unpickle[BlockBodiesStorage].fromBytes(ByteBuffer.wrap(b.toArray[Byte]))
-    //BlockBodiesStorage.fromBytes(b.toArray[Byte])
+    bytes => Unpickle[BlockBody].fromBytes(ByteBuffer.wrap(bytes.toArray[Byte]))
 
   override protected def apply(dataSource: DataSource): BlockBodiesStorage = new BlockBodiesStorage(dataSource)
 }
@@ -38,53 +36,16 @@ object BlockBodiesStorage {
 
   import boopickle.DefaultBasic._
 
-  implicit val byteStringPickler: Pickler[BlockBodyHash] = transformPickler[BlockBodyHash, Array[Byte]](ByteString(_))(_.toArray[Byte])
-  implicit val addressPickler: Pickler[Address] = generatePickler[Address]
+  implicit val byteStringPickler: Pickler[ByteString] = transformPickler[ByteString, Array[Byte]](ByteString(_))(_.toArray[Byte])
+  implicit val addressPickler: Pickler[Address] =
+    transformPickler[Address, ByteString](bytes => Address(bytes))(address => address.bytes)
   implicit val transactionPickler: Pickler[Transaction] = generatePickler[Transaction]
   implicit val ecdsaSignaturePickler: Pickler[ECDSASignature] = generatePickler[ECDSASignature]
   implicit val signedTransactionPickler: Pickler[SignedTransaction] = transformPickler[SignedTransaction, (Transaction, ECDSASignature)]
-  { case (tx, signature) => new SignedTransaction(tx, signature) }{ t => (t.tx, t.signature)}
+  { case (tx, signature) => new SignedTransaction(tx, signature) }{ stx => (stx.tx, stx.signature)}
 
   implicit val blockHeaderPickler: Pickler[BlockHeader] = generatePickler[BlockHeader]
   implicit val blockBodyPickler: Pickler[BlockBody] = transformPickler[BlockBody, (Seq[SignedTransaction], Seq[BlockHeader])]
-  {case (stx, nodes) => new BlockBody(stx, nodes) }{ b => (b.transactionList, b.uncleNodesList) }
+  {case (stx, nodes) => BlockBody(stx, nodes) }{ blockBody => (blockBody.transactionList, blockBody.uncleNodesList) }
 
-//  import io.iohk.ethereum.rlp.RLPImplicitConversions._
-//  import io.iohk.ethereum.rlp.RLPImplicits._
-//
-//  private d signedTransactionToBytes(signedTx: SignedTransaction): RLPEncodeable = {
-//    import signedTx._
-//    import signedTx.tx._
-//    RLPList(nonce, gasPrice, gasLimit, receivingAddress.map(_.toArray).getOrElse(Array.emptyByteArray): Array[Byte], value,
-//      payload, signature.v, signature.r, signature.s)
-//  }
-//
-//  private def signedTransactionFromEncodable(rlpEncodeable: RLPEncodeable): SignedTransaction = rlpEncodeable match {
-//    case RLPList(nonce, gasPrice, gasLimit, (receivingAddress: RLPValue), value,
-//    payload, pointSign, signatureRandom, signature) =>
-//      val receivingAddressOpt = if(receivingAddress.bytes.isEmpty) None else Some(Address(receivingAddress.bytes))
-//      SignedTransaction(
-//        Transaction(nonce, gasPrice, gasLimit, receivingAddressOpt, value, payload),
-//        (pointSign: Int).toByte,
-//        signatureRandom,
-//        signature
-//      )
-//  }
-
-
-//  private[BlockBodiesStorage] def toBytes(blockBody: BlockBody): IndexedSeq[Byte] = {
-//    encode(BlockBody.blockBodyToRlpEncodable(
-//      blockBody,
-//      signedTransactionToBytes,
-//      header => BlockHeaderEnc(header).toRLPEncodable
-//    ))
-//  }
-
-//  private[BlockBodiesStorage] def fromBytes(bytes: Array[Byte]): BlockBody = {
-//    BlockBody.rlpEncodableToBlockBody(
-//      rawDecode(bytes),
-//      signedTransactionFromEncodable,
-//      rlp => BlockheaderEncodableDec(rlp).toBlockHeader
-//    )
-//  }
 }
