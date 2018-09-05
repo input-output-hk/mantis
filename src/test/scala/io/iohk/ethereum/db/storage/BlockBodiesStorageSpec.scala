a4d900e86fa71d4f18b505e68247a800ddeeb9c6
Agnieszka Kowal <agnieszka.kowal@scalac.io>
2018-09-05 14:07:12 +0200

    Add pickler implicits

---
+++ src/test/scala/io/iohk/ethereum/db/storage/BlockBodiesStorageSpec.scala
@@ -0,0 +1,9 @@
+package io.iohk.ethereum.db.storage
+
+import io.iohk.ethereum.ObjectGenerators
+import org.scalatest.{ FlatSpec, FunSuite, Matchers }
+import org.scalatest.prop.PropertyChecks
+
+class BlockBodiesStorageSpec extends FlatSpec with Matchers with PropertyChecks with ObjectGenerators {
+
+}
