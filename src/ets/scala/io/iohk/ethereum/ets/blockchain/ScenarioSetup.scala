43e062279fd5a8879c25fb16831b36811d1a3daa
Agnieszka Kowal <agnieszka.kowal@scalac.io>
2018-09-19 11:25:00 +0200

    Use constantinopleValidators in config for Constantinople network in ScenerioSetup

--- src/ets/scala/io/iohk/ethereum/ets/blockchain/ScenarioSetup.scala
+++ src/ets/scala/io/iohk/ethereum/ets/blockchain/ScenarioSetup.scala
@@ -96,7 +96,7 @@ abstract class ScenarioSetup(_vm: VMImpl, scenario: BlockchainScenario) {
     case "EIP158" => (Eip158Config, Validators.eip158Validators)
     case "HomesteadToDaoAt5" => (HomesteadToDaoAt5, Validators.homesteadToDaoValidators)
     case "Byzantium" => (ByzantiumConfig, Validators.byzantiumValidators)
-    case "Constantinople" => (ConstantinopleConfig, Validators.byzantiumValidators)
+    case "Constantinople" => (ConstantinopleConfig, Validators.constantinopleValidators)
     case "EIP158ToByzantiumAt5" => (Eip158ToByzantiumAt5Config, Validators.eip158ToByzantiumValidators)
     // Some default config, test will fail or be canceled
     case _ => (FrontierConfig, Validators.frontierValidators)
