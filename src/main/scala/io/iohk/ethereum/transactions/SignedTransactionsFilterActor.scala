21b90ac079cde4d6c53d593962e34b634f69d4a2
Michal Mrozek <michal.mrozek@iohk.io>
2018-11-07 12:45:58 +0100

    FastSync - pulling head state concurrently with chain - SignedTransactionFilter parallelism

--- src/main/scala/io/iohk/ethereum/transactions/SignedTransactionsFilterActor.scala
+++ src/main/scala/io/iohk/ethereum/transactions/SignedTransactionsFilterActor.scala
@@ -1,6 +1,7 @@
 package io.iohk.ethereum.transactions
 
 import akka.actor.{Actor, ActorRef, Props}
+import akka.pattern.pipe
 import io.iohk.ethereum.domain.SignedTransaction
 import io.iohk.ethereum.network.PeerEventBusActor.PeerEvent.MessageFromPeer
 import io.iohk.ethereum.network.PeerEventBusActor.{PeerSelector, Subscribe}
@@ -8,15 +9,22 @@ import io.iohk.ethereum.network.PeerEventBusActor.SubscriptionClassifier.Message
 import io.iohk.ethereum.network.PeerId
 import io.iohk.ethereum.network.p2p.messages.CommonMessages.SignedTransactions
 import io.iohk.ethereum.transactions.SignedTransactionsFilterActor.ProperSignedTransactions
+import java.util.concurrent.Executors
+import scala.concurrent.{ExecutionContext, Future}
 
 class SignedTransactionsFilterActor(pendingTransactionsManager: ActorRef, peerEventBus: ActorRef) extends Actor{
 
   peerEventBus ! Subscribe(MessageClassifier(Set(SignedTransactions.code), PeerSelector.AllPeers))
 
+  implicit val ec = ExecutionContext.fromExecutor(Executors.newWorkStealingPool())
+
   override def receive: Receive = {
     case MessageFromPeer(SignedTransactions(newTransactions), peerId) =>
-      val correctTransactions = newTransactions.filter(tx => SignedTransaction.getSender(tx).isDefined)
-      pendingTransactionsManager ! ProperSignedTransactions(correctTransactions.toSet, peerId)
+      val correctTransactions = Future {
+        val txs = newTransactions.filter(tx => SignedTransaction.getSender(tx).isDefined).toSet
+        ProperSignedTransactions(txs, peerId)
+      }
+      correctTransactions pipeTo pendingTransactionsManager
   }
 }
 
