7cb9800cecc1983fb516294e052ad5bfa2a16c15
Mirko Alic <31509803+mirkoAlic@users.noreply.github.com>
2020-10-01 09:54:02 -0300

    [ETCM-48] Add json rpc http healthcheck (#713)

--- src/test/scala/io/iohk/ethereum/jsonrpc/server/http/JsonRpcHttpServerSpec.scala
+++ src/test/scala/io/iohk/ethereum/jsonrpc/server/http/JsonRpcHttpServerSpec.scala
@@ -7,7 +7,7 @@ import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.util.ByteString
 import ch.megard.akka.http.cors.scaladsl.model.HttpOriginMatcher
 import io.iohk.ethereum.jsonrpc.server.http.JsonRpcHttpServer.JsonRpcHttpServerConfig
-import io.iohk.ethereum.jsonrpc.{JsonRpcController, JsonRpcResponse}
+import io.iohk.ethereum.jsonrpc.{JsonRpcController, JsonRpcResponse, JsonRpcHealthChecker}
 import org.json4s.JsonAST.{JInt, JString}
 import org.scalamock.scalatest.MockFactory
 import scala.concurrent.Future
@@ -17,26 +17,32 @@ import org.scalatest.matchers.should.Matchers
 class JsonRpcHttpServerSpec extends AnyFlatSpec with Matchers with ScalatestRouteTest {
 
   it should "pass valid json request to controller" in new TestSetup {
-    (mockJsonRpcController.handleRequest _).expects(*).returning(Future.successful(JsonRpcResponse("2.0", Some(JString("this is a response")), None, JInt(1))))
+    (mockJsonRpcController.handleRequest _)
+      .expects(*)
+      .returning(Future.successful(JsonRpcResponse("2.0", Some(JString("this is a response")), None, JInt(1))))
 
     val jsonRequest = ByteString("""{"jsonrpc":"2.0", "method": "asd", "id": "1"}""")
-    val postRequest = HttpRequest(HttpMethods.POST, uri = "/", entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
+    val postRequest =
+      HttpRequest(HttpMethods.POST, uri = "/", entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
 
-    postRequest ~>  Route.seal(mockJsonRpcHttpServer.route) ~> check {
+    postRequest ~> Route.seal(mockJsonRpcHttpServer.route) ~> check {
       status shouldEqual StatusCodes.OK
       responseAs[String] shouldEqual """{"jsonrpc":"2.0","result":"this is a response","id":1}"""
     }
   }
 
   it should "pass valid batch json request to controller" in new TestSetup {
-    (mockJsonRpcController.handleRequest _).expects(*)
+    (mockJsonRpcController.handleRequest _)
+      .expects(*)
       .twice()
       .returning(Future.successful(JsonRpcResponse("2.0", Some(JString("this is a response")), None, JInt(1))))
 
-    val jsonRequest = ByteString("""[{"jsonrpc":"2.0", "method": "asd", "id": "1"}, {"jsonrpc":"2.0", "method": "asd", "id": "2"}]""")
-    val postRequest = HttpRequest(HttpMethods.POST, uri = "/", entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
+    val jsonRequest =
+      ByteString("""[{"jsonrpc":"2.0", "method": "asd", "id": "1"}, {"jsonrpc":"2.0", "method": "asd", "id": "2"}]""")
+    val postRequest =
+      HttpRequest(HttpMethods.POST, uri = "/", entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
 
-    postRequest ~>  Route.seal(mockJsonRpcHttpServer.route) ~> check {
+    postRequest ~> Route.seal(mockJsonRpcHttpServer.route) ~> check {
       status === StatusCodes.OK
       responseAs[String] shouldEqual """[{"jsonrpc":"2.0","result":"this is a response","id":1},{"jsonrpc":"2.0","result":"this is a response","id":1}]"""
     }
@@ -44,9 +50,10 @@ class JsonRpcHttpServerSpec extends AnyFlatSpec with Matchers with ScalatestRout
 
   it should "return BadRequest when malformed request is received" in new TestSetup {
     val jsonRequest = ByteString("""{"jsonrpc":"2.0", "method": "this is not a valid json""")
-    val postRequest = HttpRequest(HttpMethods.POST, uri = "/", entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
+    val postRequest =
+      HttpRequest(HttpMethods.POST, uri = "/", entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
 
-    postRequest ~>  Route.seal(mockJsonRpcHttpServer.route) ~> check {
+    postRequest ~> Route.seal(mockJsonRpcHttpServer.route) ~> check {
       status shouldEqual StatusCodes.BadRequest
     }
   }
@@ -57,26 +64,30 @@ class JsonRpcHttpServerSpec extends AnyFlatSpec with Matchers with ScalatestRout
       HttpMethods.POST,
       uri = "/",
       headers = Origin(HttpOrigin("http://non_accepted_origin.com")) :: Nil,
-      entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
+      entity = HttpEntity(MediaTypes.`application/json`, jsonRequest)
+    )
 
     import mockJsonRpcHttpServerWithCors.myRejectionHandler
-    postRequest ~>  Route.seal(mockJsonRpcHttpServerWithCors.route) ~> check {
+    postRequest ~> Route.seal(mockJsonRpcHttpServerWithCors.route) ~> check {
       status shouldEqual StatusCodes.Forbidden
     }
   }
 
   it should "accept CORS Requests" in new TestSetup {
 
-    (mockJsonRpcController.handleRequest _).expects(*).returning(Future.successful(JsonRpcResponse("2.0", Some(JString("this is a response")), None, JInt(1))))
+    (mockJsonRpcController.handleRequest _)
+      .expects(*)
+      .returning(Future.successful(JsonRpcResponse("2.0", Some(JString("this is a response")), None, JInt(1))))
 
     val jsonRequest = ByteString("""{"jsonrpc":"2.0", "method": "eth_blockNumber", "id": "1"}""")
     val postRequest = HttpRequest(
       HttpMethods.POST,
       uri = "/",
       headers = Origin(corsAllowedOrigin) :: Nil,
-      entity = HttpEntity(MediaTypes.`application/json`, jsonRequest))
+      entity = HttpEntity(MediaTypes.`application/json`, jsonRequest)
+    )
 
-    postRequest ~>  Route.seal(mockJsonRpcHttpServerWithCors.route) ~> check {
+    postRequest ~> Route.seal(mockJsonRpcHttpServerWithCors.route) ~> check {
       status shouldEqual StatusCodes.OK
     }
   }
@@ -94,8 +105,10 @@ class JsonRpcHttpServerSpec extends AnyFlatSpec with Matchers with ScalatestRout
     }
 
     val mockJsonRpcController = mock[JsonRpcController]
+    val mockJsonRpcHealthChecker = mock[JsonRpcHealthChecker]
     val mockJsonRpcHttpServer = new JsonRpcHttpServer {
-      val jsonRpcController = mockJsonRpcController
+      override val jsonRpcController = mockJsonRpcController
+      override val jsonRpcHealthChecker = mockJsonRpcHealthChecker
 
       def run(): Unit = ()
 
@@ -105,7 +118,8 @@ class JsonRpcHttpServerSpec extends AnyFlatSpec with Matchers with ScalatestRout
     val corsAllowedOrigin = HttpOrigin("http://localhost:3333")
 
     val mockJsonRpcHttpServerWithCors = new JsonRpcHttpServer {
-      val jsonRpcController = mockJsonRpcController
+      override val jsonRpcController = mockJsonRpcController
+      override val jsonRpcHealthChecker = mockJsonRpcHealthChecker
 
       def run(): Unit = ()
 
