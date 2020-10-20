c74f480ef92734b906aaa6205527735777bbd998
Michal Mrozek <michal.mrozek@iohk.io>
2020-10-20 13:36:16 +0200

    [ETCM-202] Fix scalastyle

--- src/main/scala/io/iohk/ethereum/cli/CliLauncher.scala
+++ src/main/scala/io/iohk/ethereum/cli/CliLauncher.scala
@@ -2,6 +2,7 @@ package io.iohk.ethereum.cli
 
 import com.monovore.decline._
 
+//scalastyle:off
 object CliLauncher extends App {
 
   CliCommands.api.map(println).parse(PlatformApp.ambientArgs getOrElse args, sys.env) match {
