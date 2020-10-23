effc77b8769489fa72f391705859e7a5e179245e
lemastero <piotr.paradzinski@iohk.io>
2020-10-23 15:22:16 +0200

    [ETCM-269] add AkkaAsk in DebugService

--- src/main/scala/io/iohk/ethereum/jsonrpc/DebugService.scala
+++ src/main/scala/io/iohk/ethereum/jsonrpc/DebugService.scala
@@ -1,53 +1,44 @@
 package io.iohk.ethereum.jsonrpc
 
 import akka.actor.ActorRef
-import akka.pattern._
 import akka.util.Timeout
+import io.iohk.ethereum.jsonrpc.AkkaTaskOps._
 import io.iohk.ethereum.jsonrpc.DebugService.{ListPeersInfoRequest, ListPeersInfoResponse}
 import io.iohk.ethereum.network.EtcPeerManagerActor.{PeerInfo, PeerInfoResponse}
 import io.iohk.ethereum.network.PeerManagerActor.Peers
 import io.iohk.ethereum.network.{EtcPeerManagerActor, Peer, PeerActor, PeerId, PeerManagerActor}
 import monix.eval.Task
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
 object DebugService {
-
   case class ListPeersInfoRequest()
   case class ListPeersInfoResponse(peers: List[PeerInfo])
-
 }
 
 class DebugService(peerManager: ActorRef, etcPeerManager: ActorRef) {
 
   def listPeersInfo(getPeersInfoRequest: ListPeersInfoRequest): ServiceResponse[ListPeersInfoResponse] = {
-    val result = for {
+    for {
       ids <- getPeerIds
       peers <- Task.traverse(ids)(getPeerInfo)
-    } yield ListPeersInfoResponse(peers.flatten)
-
-    Task.from(result.map(Right(_)))
+    } yield Right(ListPeersInfoResponse(peers.flatten))
   }
 
   private def getPeerIds: Task[List[PeerId]] = {
     implicit val timeout: Timeout = Timeout(5.seconds)
 
-    Task.fromFuture(
-      (peerManager ? PeerManagerActor.GetPeers)
-        .mapTo[Peers]
-        .recover { case _ => Peers(Map.empty[Peer, PeerActor.Status]) }
-        .map(_.peers.keySet.map(_.id).toList)
-    )
+    peerManager
+      .askFor[Peers](PeerManagerActor.GetPeers)
+      .onErrorRecover { case _ => Peers(Map.empty[Peer, PeerActor.Status]) }
+      .map(_.peers.keySet.map(_.id).toList)
   }
 
   private def getPeerInfo(peer: PeerId): Task[Option[PeerInfo]] = {
     implicit val timeout: Timeout = Timeout(5.seconds)
 
-    Task.fromFuture {
-      (etcPeerManager ? EtcPeerManagerActor.PeerInfoRequest(peer))
-        .mapTo[PeerInfoResponse]
-        .collect { case PeerInfoResponse(info) => info }
-    }
+    etcPeerManager
+      .askFor[PeerInfoResponse](EtcPeerManagerActor.PeerInfoRequest(peer))
+      .map(resp => resp.peerInfo)
   }
 }
