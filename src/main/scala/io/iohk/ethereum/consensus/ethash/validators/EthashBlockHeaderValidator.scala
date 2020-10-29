25256707139652b3ae0710aa7a9c703f0b7c45ef
Michal Mrozek <michal.mrozek@iohk.io>
2020-10-29 18:10:37 +0100

    [ETCM-109] Update cache handling in EthashBlockHeaderValidator

--- src/main/scala/io/iohk/ethereum/consensus/ethash/validators/EthashBlockHeaderValidator.scala
+++ src/main/scala/io/iohk/ethereum/consensus/ethash/validators/EthashBlockHeaderValidator.scala
@@ -9,6 +9,7 @@ import io.iohk.ethereum.consensus.validators.{BlockHeaderError, BlockHeaderValid
 import io.iohk.ethereum.crypto
 import io.iohk.ethereum.domain.BlockHeader
 import io.iohk.ethereum.utils.BlockchainConfig
+import monix.execution.atomic.{Atomic, AtomicAny}
 
 /**
   * A block header validator for Ethash.
@@ -18,9 +19,8 @@ class EthashBlockHeaderValidator(blockchainConfig: BlockchainConfig)
   import EthashBlockHeaderValidator._
 
   // NOTE the below comment is from before PoW decoupling
-  // we need concurrent map since validators can be used from multiple places
-  protected val powCaches: java.util.concurrent.ConcurrentMap[Long, PowCacheData] =
-    new java.util.concurrent.ConcurrentHashMap[Long, PowCacheData]()
+  // we need atomic since validators can be used from multiple places
+  protected val powCaches: AtomicAny[List[PowCacheData]] = Atomic(List.empty[PowCacheData])
 
   protected def difficulty: DifficultyCalculator = new EthashDifficultyCalculator(blockchainConfig)
 
@@ -40,21 +40,15 @@ class EthashBlockHeaderValidator(blockchainConfig: BlockchainConfig)
   protected def validatePoW(blockHeader: BlockHeader): Either[BlockHeaderError, BlockHeaderValid] = {
     import EthashUtils._
 
-    import scala.collection.JavaConverters._
-
     def getPowCacheData(epoch: Long, seed: ByteString): PowCacheData = {
-      Option(powCaches.get(epoch)) match {
-        case Some(pcd) => pcd
-        case None =>
-          val data = new PowCacheData(cache = EthashUtils.makeCache(epoch, seed), dagSize = EthashUtils.dagSize(epoch))
-
-          val keys = powCaches.keySet().asScala
-          val keysToRemove = keys.toSeq.sorted.take(keys.size - MaxPowCaches + 1)
-          keysToRemove.foreach(powCaches.remove)
-
-          powCaches.put(epoch, data)
-
-          data
+      powCaches.transformAndExtract { cache =>
+        cache.find(_.epoch == epoch) match {
+          case Some(pcd) => (pcd, cache)
+          case None =>
+            val data =
+              PowCacheData(epoch, cache = EthashUtils.makeCache(epoch, seed), dagSize = EthashUtils.dagSize(epoch))
+            (data, (data :: cache).take(MaxPowCaches))
+        }
       }
     }
 
@@ -78,5 +72,5 @@ class EthashBlockHeaderValidator(blockchainConfig: BlockchainConfig)
 object EthashBlockHeaderValidator {
   final val MaxPowCaches: Int = 2 // maximum number of epochs for which PoW cache is stored in memory
 
-  class PowCacheData(val cache: Array[Int], val dagSize: Long)
+  case class PowCacheData(epoch: Long, cache: Array[Int], dagSize: Long)
 }
