6c26ce5b5a468eee2c43878ab7aaafb0def8220b
lemastero <piotr.paradzinski@iohk.io>
2020-11-17 14:37:08 +0100

    [ETCM-126] revert accidental change

--- src/main/scala/io/iohk/ethereum/jsonrpc/JsonRpcController.scala
+++ src/main/scala/io/iohk/ethereum/jsonrpc/JsonRpcController.scala
@@ -195,7 +195,7 @@ class JsonRpcController(
         req
       )
     case req @ JsonRpcRequest(_, "eth_pendingTransactions", _, _) =>
-      handle[git EthPendingTransactionsRequest, EthPendingTransactionsResponse](ethService.ethPendingTransactions, req)
+      handle[EthPendingTransactionsRequest, EthPendingTransactionsResponse](ethService.ethPendingTransactions, req)
   }
 
   private def handleDebugRequest: PartialFunction[JsonRpcRequest, Task[JsonRpcResponse]] = {
