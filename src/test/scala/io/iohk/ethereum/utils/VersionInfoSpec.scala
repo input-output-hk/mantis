8eff0470cbbdd06a6fd0ebdc0bb7eac9f5a399f0
Akosh Farkash <akosh.farkash@iohk.io>
2020-11-18 14:50:22 +0000

    ETCM-365: More precise regex in test.

--- src/test/scala/io/iohk/ethereum/utils/VersionInfoSpec.scala
+++ src/test/scala/io/iohk/ethereum/utils/VersionInfoSpec.scala
@@ -6,11 +6,13 @@ import org.scalatest.matchers.should.Matchers
 class VersionInfoSpec extends AnyFlatSpec with Matchers {
   behavior of "nodeName"
 
-  it should "preserve major and minor Java version" in {
-    VersionInfo.nodeName() should fullyMatch regex """mantis/v\d.*-[a-z0-9]{7}/.+-.+/.+-.+-java-\d+\.\d+.*"""
+  it should "match ethstats expected structure and preserve major and minor Java version" in {
+    VersionInfo.nodeName() should fullyMatch regex """mantis/v\d(\.\d+)*-[a-z0-9]{7}/[^/]+-[^/]+/[^/]+-.[^/]+-java-\d+\.\d+(\.\d)*"""
   }
 
   it should "augment the name with an identity" in {
-    VersionInfo.nodeName(Some("iohk")) should startWith("mantis/iohk/")
+    val name = VersionInfo.nodeName(Some("iohk"))
+    name should startWith("mantis/iohk/v")
+    name.count(_ == '/') shouldBe 4
   }
 }
