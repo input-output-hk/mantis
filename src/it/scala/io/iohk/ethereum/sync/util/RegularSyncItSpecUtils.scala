071356b69ffdb0d27a0de73485d8d13611d0cf0c
mk <mirko.alic@iohk.io>
2020-11-19 17:38:08 -0300

    [ETCM-283] Fix it

--- src/it/scala/io/iohk/ethereum/sync/util/RegularSyncItSpecUtils.scala
+++ src/it/scala/io/iohk/ethereum/sync/util/RegularSyncItSpecUtils.scala
@@ -3,6 +3,7 @@ package io.iohk.ethereum.sync.util
 import akka.actor.ActorRef
 import akka.util.ByteString
 import cats.effect.Resource
+import io.iohk.ethereum.Mocks.MockValidatorsAlwaysSucceed
 import io.iohk.ethereum.blockchain.sync.{PeersClient, SyncProtocol}
 import io.iohk.ethereum.blockchain.sync.regular.BlockBroadcasterActor.BroadcastBlock
 import io.iohk.ethereum.blockchain.sync.regular.RegularSync
