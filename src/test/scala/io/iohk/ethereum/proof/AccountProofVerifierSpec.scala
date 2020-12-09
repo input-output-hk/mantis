52cdfa2f02fbbf645c2c3b3effadcfaf182e056f
lemastero <piotr.paradzinski@iohk.io>
2020-12-09 15:34:10 +0100

    [ETCM-468] add spec for account proof veirfier + replace Try with Either

---
+++ src/test/scala/io/iohk/ethereum/proof/AccountProofVerifierSpec.scala
@@ -0,0 +1,8 @@
+package io.iohk.ethereum.proof
+
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.must.Matchers
+
+class AccountProofVerifierSpec extends AnyFlatSpecLike with Matchers {
+  // TODO PP
+}
