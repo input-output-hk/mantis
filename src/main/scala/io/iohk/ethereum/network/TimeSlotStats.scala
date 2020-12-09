657ff6237f117a5d619038bb49483d9cf35fd2ef
Akosh Farkash <akosh.farkash@iohk.io>
2020-12-09 17:24:52 +0000

    ETCM-463: Add TimeSlotStats.

---
+++ src/main/scala/io/iohk/ethereum/network/TimeSlotStats.scala
@@ -0,0 +1,107 @@
+package io.iohk.ethereum.network
+
+import cats._
+import cats.implicits._
+import scala.concurrent.duration.FiniteDuration
+
+/** Track statistics over time. */
+case class TimeSlotStats[K, V: Monoid] private (
+    // Time resolution.
+    slotDuration: FiniteDuration,
+    slotCount: Int,
+    // The last written slot.
+    lastIdx: Int,
+    // Ring buffer of the timestamp of each slot.
+    timeSlots: IndexedSeq[TimeSlotStats.Timestamp],
+    // Ring buffer of statistics per slot.
+    statSlots: IndexedSeq[Map[K, V]]
+) {
+  import TimeSlotStats._
+
+  private def succ(idx: Int): Int = (idx + 1) % slotCount
+  private def pred(idx: Int): Int = (idx - 1) % slotCount
+
+  /** Merge new stats for a given key in the current timestamp. */
+  def add(key: K, stat: V, timestamp: Timestamp = System.currentTimeMillis): TimeSlotStats[K, V] = {
+    val currSlot = slotOf(timestamp)
+    val lastSlot = timeSlots(lastIdx)
+    if (currSlot == lastSlot) {
+      // We're still in the same timeslot, so just append stats.
+      val newStats = statSlots(lastIdx) |+| Map(key -> stat)
+      copy(
+        statSlots = statSlots.updated(lastIdx, newStats)
+      )
+    } else if (currSlot > lastSlot) {
+      // Go to the next slot.
+      val newStats = Map(key -> stat)
+      val newIdx = succ(lastIdx)
+      copy(
+        lastIdx = newIdx,
+        timeSlots = timeSlots.updated(newIdx, currSlot),
+        statSlots = statSlots.updated(newIdx, newStats)
+      )
+    } else {
+      // Some previous slot, just ignore it.
+      this
+    }
+  }
+
+  /** Forget all statistics about a given key. */
+  def remove(key: K): TimeSlotStats[K, V] =
+    copy(statSlots = statSlots.map(_ - key))
+
+  /** Aggregate stats for a key in all slots that are within the duration. */
+  def get(key: K, timestamp: Timestamp = System.currentTimeMillis): V =
+    fold(timestamp)(Monoid[V].empty) { case (acc, stats) =>
+      acc |+| stats.getOrElse(key, Monoid[V].empty)
+    }
+
+  /** Aggregate all stats in all slots within the duration. */
+  def getAll(timestamp: Timestamp = System.currentTimeMillis): Map[K, V] =
+    fold(timestamp)(Map.empty[K, V]) { case (acc, stats) =>
+      acc |+| stats
+    }
+
+  private def fold[A](timestamp: Timestamp)(init: A)(f: (A, Map[K, V]) => A) = {
+    val (start, end) = slotRange(timestamp)
+
+    def loop(idx: Int, acc: A): A = {
+      val t = timeSlots(idx)
+      if (t == 0 || t < start || t > end)
+        acc
+      else
+        loop(pred(idx), f(acc, statSlots(idx)))
+    }
+
+    loop(lastIdx, init)
+  }
+
+  /** Truncate the current timestamp based on the slot duration. */
+  private def slotOf(timestamp: Timestamp): Timestamp = {
+    timestamp - timestamp % slotDuration.toMillis
+  }
+
+  private def slotRange(timestamp: Timestamp): (Timestamp, Timestamp) = {
+    val endSlot = slotOf(timestamp)
+    val startSlot = slotOf(timestamp - slotDuration.toMillis * slotCount)
+    startSlot -> endSlot
+  }
+}
+
+object TimeSlotStats {
+
+  // Milliseconds since epoch.
+  type Timestamp = Long
+
+  def apply[K, V: Monoid](
+      slotDuration: FiniteDuration,
+      slotCount: Int
+  ): TimeSlotStats[K, V] =
+    TimeSlotStats[K, V](
+      slotDuration,
+      slotCount,
+      lastIdx = slotCount - 1, // So the first slot we fill will move to 0.
+      timeSlots = IndexedSeq.fill(slotCount)(0L),
+      statSlots = IndexedSeq.fill(slotCount)(Map.empty[K, V])
+    )
+}
---
+++ src/test/scala/io/iohk/ethereum/network/TimeSlotStatsSpec.scala
@@ -0,0 +1,95 @@
+package io.iohk.ethereum.network
+
+import org.scalatest.flatspec.AnyFlatSpec
+import scala.concurrent.duration._
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.Inspectors
+
+class TimeSlotStatsSpec extends AnyFlatSpec with Matchers {
+  behavior of "TimeSlotStats"
+
+  val emptyStats = TimeSlotStats[String, Int](slotDuration = 1.minute, slotCount = 30)
+
+  it should "add new keys to the last timeslot" in {
+    val stats = emptyStats.add("foo", 1)
+    stats.statSlots(0)("foo") shouldBe 1
+  }
+
+  it should "merge keys in the last timeslot" in {
+    val stats = emptyStats.add("foo", 1).add("foo", 2).add("bar", 0, timestamp = System.currentTimeMillis + 10)
+    stats.statSlots(0)("foo") shouldBe 3
+    stats.statSlots(0)("bar") shouldBe 0
+  }
+
+  it should "ignore updates for earlier timeslots" in {
+    val stats0 = emptyStats.add("foo", 1)
+    val stats1 = stats0.add("foo", 2, timestamp = System.currentTimeMillis - emptyStats.slotDuration.toMillis - 1)
+
+    stats0 shouldBe stats1
+  }
+
+  it should "add new slots for the next timeslot" in {
+    val stats = emptyStats
+      .add("foo", 1)
+      .add("foo", 2, timestamp = System.currentTimeMillis + emptyStats.slotDuration.toMillis + 1)
+
+    stats.statSlots(0)("foo") shouldBe 1
+    stats.statSlots(1)("foo") shouldBe 2
+  }
+
+  it should "remove keys from all slots" in {
+    val stats = emptyStats
+      .add("foo", 1)
+      .add("bar", 2)
+      .add("foo", 3, timestamp = System.currentTimeMillis + emptyStats.slotDuration.toMillis + 1)
+      .add("bar", 4, timestamp = System.currentTimeMillis + emptyStats.slotDuration.toMillis + 2)
+      .remove("foo")
+
+    Inspectors.forAll(stats.statSlots) {
+      _ should not contain key("foo")
+    }
+  }
+
+  it should "turn around and overwrite the first slot after all of them have been written to" in {
+    val stats = Range
+      .inclusive(0, emptyStats.slotCount)
+      .map(i => i -> (System.currentTimeMillis + emptyStats.slotDuration.toMillis * i))
+      .foldLeft(emptyStats) { case (stats, (i, timestamp)) =>
+        stats.add("foo", i, timestamp)
+      }
+
+    stats.statSlots(0)("foo") shouldBe emptyStats.slotCount
+    stats.statSlots(1)("foo") shouldBe 1
+  }
+
+  it should "aggregate the stats of a given key" in new AggregateFixture {
+    stats.get("foo") shouldBe 0
+    stats.get("foo", ts1) shouldBe 0
+    stats.get("bar", ts1) shouldBe 0
+    stats.get("foo", ts2) shouldBe 3
+    stats.get("bar", ts2) shouldBe 6
+    stats.get("BAR", ts2) shouldBe 0
+  }
+
+  it should "aggregate all stats" in new AggregateFixture {
+    stats.getAll() shouldBe empty
+    stats.getAll(ts1) shouldBe empty
+    stats.getAll(ts2) shouldBe Map("foo" -> 3, "bar" -> 6)
+  }
+
+  trait AggregateFixture {
+    val slotMillis = emptyStats.slotDuration.toMillis
+    val ts0 = System.currentTimeMillis
+    val ts1 = ts0 + slotMillis * emptyStats.slotCount // puts t0 out of scope
+    val ts2 = ts1 + slotMillis
+
+    val stats = emptyStats
+      .add("foo", 1, ts0)
+      .add("bar", 2, ts1)
+      .add("foo", 3, ts2)
+      .add("bar", 4, ts2)
+
+    stats.lastIdx shouldBe 2
+  }
+
+}
