40d9731bdfdcf60e4f43c9b4bc7e691bb5dc215d
Akosh Farkash <akosh.farkash@iohk.io>
2020-12-10 15:29:59 +0000

    ETCM-463: Use a dedicated Stat with a responsesReceived rather than an Int.

--- src/main/scala/io/iohk/ethereum/network/PeerStatisticsActor.scala
+++ src/main/scala/io/iohk/ethereum/network/PeerStatisticsActor.scala
@@ -4,6 +4,7 @@ import akka.actor._
 import io.iohk.ethereum.network.PeerEventBusActor._
 import io.iohk.ethereum.network.p2p.messages.Codes
 import scala.concurrent.duration.FiniteDuration
+import cats.kernel.Monoid
 
 class PeerStatisticsActor(
     peerEventBus: ActorRef,
@@ -11,15 +12,16 @@ class PeerStatisticsActor(
 ) extends Actor {
   import PeerStatisticsActor._
 
-  // Subscribe to messages received from peers to maintain stats.
+  // Subscribe to messages received from handshaked peers to maintain stats.
   peerEventBus ! Subscribe(MessageSubscriptionClassifier)
+  // Removing peers is an optimisation to free space, but eventualy the stats would be overwritten anyway.
   peerEventBus ! Subscribe(SubscriptionClassifier.PeerDisconnectedClassifier(PeerSelector.AllPeers))
 
   def receive: Receive = handlePeerEvents orElse handleStatsRequests
 
   private def handlePeerEvents: Receive = {
     case PeerEvent.MessageFromPeer(_, peerId) =>
-      maybeStats = maybeStats.map(_.add(peerId, 1))
+      maybeStats = maybeStats.map(_.add(peerId, Stat(responsesReceived = 1)))
 
     case PeerEvent.PeerDisconnected(peerId) =>
       maybeStats = maybeStats.map(_.remove(peerId))
@@ -30,12 +32,20 @@ class PeerStatisticsActor(
       sender ! StatsForAll(maybeStats.map(_.getAll()).getOrElse(Map.empty))
 
     case GetStatsForPeer(peerId) =>
-      sender ! StatsForPeer(peerId, maybeStats.map(_.get(peerId)).getOrElse(0))
+      sender ! StatsForPeer(peerId, maybeStats.map(_.get(peerId)).getOrElse(Stat.empty))
   }
 }
 
 object PeerStatisticsActor {
-  type Stat = Int
+  case class Stat(
+      responsesReceived: Int
+  )
+  object Stat {
+    val empty: Stat = Stat(0)
+
+    implicit val monoid: Monoid[Stat] =
+      Monoid.instance(empty, (a, b) => Stat(a.responsesReceived + b.responsesReceived))
+  }
 
   def props(peerEventBus: ActorRef, slotDuration: FiniteDuration, slotCount: Int): Props =
     Props(
