f7e0a697e711fdb3a9817ea9aa01f848193e1be2
Dmitry Voronov <dmitry.voronov@iohk.io>
2020-12-24 23:01:17 +0300

    Formatting applied

--- src/main/scala/io/iohk/ethereum/jsonrpc/server/http/RateLimit.scala
+++ src/main/scala/io/iohk/ethereum/jsonrpc/server/http/RateLimit.scala
@@ -10,7 +10,6 @@ import de.heikoseeberger.akkahttpjson4s.Json4sSupport
 import io.iohk.ethereum.jsonrpc.serialization.JsonSerializers
 import org.json4s.{DefaultFormats, Formats, Serialization, native}
 
-
 class RateLimit(config: RateLimitConfig) extends Directive0 with Json4sSupport {
 
   private implicit val serialization: Serialization = native.Serialization
@@ -23,16 +22,16 @@ class RateLimit(config: RateLimitConfig) extends Directive0 with Json4sSupport {
 
   // It determines whether a request needs to be blocked
   // Such algebras prevent if-elseif-else boilerplate in the JsonRPCServer code
-  override def tapply(f: Unit => Route): Route =  {
-    if (config.enabled) { 
+  override def tapply(f: Unit => Route): Route = {
+    if (config.enabled) {
       val minInterval = config.minRequestInterval.toSeconds
-      extractClientIP { ip => 
-        if (lru.checkAndRefreshEntry(ip)) { 
+      extractClientIP { ip =>
+        if (lru.checkAndRefreshEntry(ip)) {
           val err = JsonRpcError.RateLimitError(minInterval)
           complete((StatusCodes.TooManyRequests, err))
-        } else f.apply( () )
+        } else f.apply(())
       }
-    } else f.apply( () )
+    } else f.apply(())
   }
 
 }
