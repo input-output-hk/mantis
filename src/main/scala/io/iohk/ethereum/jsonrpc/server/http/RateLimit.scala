0929963f29cb4c95558316ca14f012f24f5bb38a
Dmitry Voronov <dmitry.voronov@iohk.io>
2020-12-30 18:42:44 +0300

    Extracted blocker method

--- src/main/scala/io/iohk/ethereum/jsonrpc/server/http/RateLimit.scala
+++ src/main/scala/io/iohk/ethereum/jsonrpc/server/http/RateLimit.scala
@@ -19,7 +19,7 @@ class RateLimit(config: RateLimitConfig) extends Directive0 with Json4sSupport {
   private implicit val serialization: Serialization = native.Serialization
   private implicit val formats: Formats = DefaultFormats + JsonSerializers.RpcErrorJsonSerializer
 
-  protected def getCurrentTimeNanos: Long = System.nanoTime()
+  private[this] lazy val minInterval = config.minRequestInterval.toSeconds
 
   private[this] lazy val lru = {
     val nanoDuration = config.minRequestInterval.toNanos
@@ -35,6 +35,21 @@ class RateLimit(config: RateLimitConfig) extends Directive0 with Json4sSupport {
       .build[RemoteAddress, NotUsed]()
   }
 
+  private[this] def isBelowRateLimit(ip: RemoteAddress): Boolean = {
+    var exists = true
+    lru.get(
+      ip,
+      () => {
+        exists = false
+        NotUsed
+      }
+    )
+    exists
+  }
+
+  // Override this to test
+  protected def getCurrentTimeNanos: Long = System.nanoTime()
+
   // Such algebras prevent if-elseif-else boilerplate in the JsonRPCServer code
   // It is also guaranteed that:
   //   1) no IP address is extracted unless config.enabled is true
@@ -42,19 +57,8 @@ class RateLimit(config: RateLimitConfig) extends Directive0 with Json4sSupport {
   //   3) cache is accessed only once (using get)
   override def tapply(f: Unit => Route): Route = {
     if (config.enabled) {
-      val minInterval = config.minRequestInterval.toSeconds
       extractClientIP { ip =>
-        var exists = true
-        // We can avoid using var
-        // But in this case we access our LRU twice.
-        lru.get(
-          ip,
-          () => {
-            exists = false
-            NotUsed
-          }
-        )
-        if (exists) {
+        if (isBelowRateLimit(ip)) {
           val err = JsonRpcError.RateLimitError(minInterval)
           complete((StatusCodes.TooManyRequests, err))
         } else {
