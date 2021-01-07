bdea05a414e50a20e3c4facff79b7e5cc88b03b5
Maximiliano Biandratti <maximiliano.biandratti@iohk.io>
2021-01-07 11:41:37 -0300

    change monitoring script

--- docker/monitoring/build.sh
+++ docker/monitoring/build.sh
@@ -6,6 +6,6 @@ cd $HERE/../../
 sbt docker:publishLocal
 
 cd $HERE
-docker build -t prometheus -f ./prometheus/Dockerfile ./$HERE/prometheus/
-docker build -t grafana -f ./grafana/Dockerfile ./$HERE/grafana/
+docker build -t prometheus -f ./prometheus/Dockerfile ./prometheus/
+docker build -t grafana -f ./grafana/Dockerfile ./grafana/
 docker-compose -f docker-compose.yml up -d
