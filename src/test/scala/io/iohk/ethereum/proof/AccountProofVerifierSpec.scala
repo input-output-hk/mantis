28915f68dfaf0d62e6e524b12d77886cc77c54c2
Piotr Paradziński <piotr.paradzinski@iohk.io>
2021-01-08 01:42:39 +0100

    [ETCM-468] drop unused AccountProofVerifierSpec it is tested in combination with getProof
    Signed-off-by: Piotr Paradziński <piotr.paradzinski@iohk.io>

--- src/test/scala/io/iohk/ethereum/proof/AccountProofVerifierSpec.scala
+++
@@ -1,8 +0,0 @@
-package io.iohk.ethereum.proof
-
-import org.scalatest.flatspec.AnyFlatSpecLike
-import org.scalatest.matchers.must.Matchers
-
-class AccountProofVerifierSpec extends AnyFlatSpecLike with Matchers {
-  // TODO PP
-}
