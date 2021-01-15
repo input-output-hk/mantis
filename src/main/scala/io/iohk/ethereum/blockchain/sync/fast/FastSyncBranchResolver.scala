0020d1c17cb0431059332c386ac38db675cae745
Maximiliano Biandratti <maximiliano.biandratti@iohk.io>
2021-01-14 21:46:27 -0300

    Added ut in class FastSyncBranchResolver

--- src/main/scala/io/iohk/ethereum/blockchain/sync/fast/FastSyncBranchResolver.scala
+++ src/main/scala/io/iohk/ethereum/blockchain/sync/fast/FastSyncBranchResolver.scala
@@ -11,10 +11,12 @@ import io.iohk.ethereum.network.Peer
 import io.iohk.ethereum.network.p2p.messages.Codes
 import io.iohk.ethereum.network.p2p.messages.PV62.{BlockHeaders, GetBlockHeaders}
 import io.iohk.ethereum.utils.Config.SyncConfig
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
 class FastSyncBranchResolver(
+    val fastSync: ActorRef,
     val peerEventBus: ActorRef,
     val etcPeerManager: ActorRef,
     val blockchain: Blockchain,
@@ -28,30 +30,30 @@ class FastSyncBranchResolver(
 
   import syncConfig._
 
-  val initiator: ActorRef = context.parent
+  def handleCommonMessages: Receive = handlePeerListMessages orElse handleBlacklistMessages
 
-  override def receive: Receive = { case StartBranchResolver() =>
+  override def receive: Receive = handleCommonMessages orElse { case StartBranchResolver =>
     getBestPeer match {
       case Some((peer, _)) => requestBlockHeadersGap(peer, blockchain.getBestBlockNumber())
       case None =>
         log.info(s"Still waiting for some peer, rescheduling StartBranchResolver")
-        scheduler.scheduleOnce(1.second, self, StartBranchResolver())
+        scheduler.scheduleOnce(1.second, self, StartBranchResolver)
     }
   }
 
-  private def waitingBlockHeadersGap(bestBlockNumber: BigInt): Receive = {
-    case ResponseReceived(peer, BlockHeaders(blockHeaders), timeTaken) =>
+  private def waitingBlockHeadersGap(bestBlockNumber: BigInt): Receive = handleCommonMessages orElse {
+    case ResponseReceived(peer, BlockHeaders(blockHeaders), timeTaken) => {
       log.info("*** Received {} block headers in {} ms ***", blockHeaders.size, timeTaken)
       getLastCorrectBlock(blockHeaders, bestBlockNumber) match {
         case Some(lastValidBlockHeader) =>
-          discardLastInvalidBlocks(bestBlockNumber, lastValidBlockHeader)
-          initiator ! BranchResolvedSuccessful()
+          finalizeBranchResolver(bestBlockNumber, lastValidBlockHeader)
         case None => requestBlockHeader(peer, SearchState(0, bestBlockNumber))
       }
+    }
     case RequestFailed(peer, reason) => handleRequestFailure(peer, sender(), reason)
   }
 
-  private def binarySearchingLastValidBlock(searchState: SearchState): Receive = {
+  private def binarySearchingLastValidBlock(searchState: SearchState): Receive = handleCommonMessages orElse {
     case ResponseReceived(peer, BlockHeaders(blockHeaders), timeTaken) =>
       log.info("*** Received {} block headers in {} ms ***", blockHeaders.size, timeTaken)
       for {
@@ -69,11 +71,17 @@ class FastSyncBranchResolver(
       peer: Peer
   ): Unit = {
     if (validateBlockHeaders(parentBlockHeader, childBlockHeader)) {
-      if (childBlockHeader.number == searchState.minBlockNumber) {
-        discardLastInvalidBlocks(blockchain.getBestBlockNumber(), childBlockHeader.number)
-        initiator ! BranchResolvedSuccessful()
-      } else requestBlockHeader(peer, searchState.copy(minBlockNumber = childBlockHeader.number + 1))
+      if (childBlockHeader.number == searchState.minBlockNumber)
+        finalizeBranchResolver(blockchain.getBestBlockNumber(), childBlockHeader.number)
+      else requestBlockHeader(peer, searchState.copy(minBlockNumber = childBlockHeader.number + 1))
     } else requestBlockHeader(peer, searchState.copy(maxBlockNumber = childBlockHeader.number))
+  }
+
+  private def finalizeBranchResolver(lastBlock: BigInt, lastValidBlock: BigInt): Unit = {
+    discardLastInvalidBlocks(lastBlock, lastValidBlock)
+    context become receive
+    log.info("branch resolver finished")
+    fastSync ! BranchResolvedSuccessful
   }
 
   private def getBestPeer: Option[(Peer, PeerInfo)] = {
@@ -108,16 +116,16 @@ class FastSyncBranchResolver(
     context watch handler
   }
 
-  private def getLastCorrectBlock(candidateHeaders: Seq[BlockHeader], bestBlock: BigInt): Option[BigInt] = {
-    candidateHeaders
-      .zip(bestBlock to ((bestBlock - blockHeadersPerRequest) max 1) by -1)
-      .find { case (parent, childNumber) =>
-        blockchain.getBlockHeaderByNumber(childNumber).exists { child => validateBlockHeaders(parent, child) }
+  private def getLastCorrectBlock(candidateHeaders: Seq[BlockHeader], bestBlockNumber: BigInt): Option[BigInt] = {
+    candidateHeaders.reverse
+      .zip(bestBlockNumber to ((bestBlockNumber - blockHeadersPerRequest) max 1) by -1)
+      .find { case (child, childNumber) =>
+        blockchain.getBlockHeaderByNumber(childNumber).exists { parent => validateBlockHeaders(child, parent) }
       }
       .map(_._2)
   }
 
-  private def validateBlockHeaders(parentBlockHeader: BlockHeader, childBlockHeader: BlockHeader): Boolean =
+  private def validateBlockHeaders(childBlockHeader: BlockHeader, parentBlockHeader: BlockHeader): Boolean =
     parentBlockHeader.hash == childBlockHeader.parentHash && parentBlockHeader.number + 1 == childBlockHeader.number
 
   private def handleRequestFailure(peer: Peer, handler: ActorRef, reason: String): Unit = {
@@ -126,11 +134,11 @@ class FastSyncBranchResolver(
     if (handshakedPeers.contains(peer)) {
       blacklist(peer.id, blacklistDuration, reason) //Only in blacklist??
     }
-    self ! StartBranchResolver()
+    self ! StartBranchResolver
   }
 
   private def discardLastInvalidBlocks(lastBlock: BigInt, lastValidBlock: BigInt): Unit = {
-    (lastBlock to (lastValidBlock max 1) by -1).foreach { n =>
+    (lastBlock to (lastValidBlock + 1) by -1).foreach { n =>
       blockchain.getBlockHeaderByNumber(n).foreach { headerToRemove =>
         blockchain.removeBlock(headerToRemove.hash, withState = false)
       }
@@ -142,6 +150,7 @@ class FastSyncBranchResolver(
 
 object FastSyncBranchResolver {
   def prop(
+      fastSync: ActorRef,
       peerEventBus: ActorRef,
       etcPeerManager: ActorRef,
       blockchain: Blockchain,
@@ -149,15 +158,25 @@ object FastSyncBranchResolver {
       appStateStorage: AppStateStorage,
       scheduler: Scheduler
   ): Props =
-    Props(new FastSyncBranchResolver(peerEventBus, etcPeerManager, blockchain, syncConfig, appStateStorage, scheduler))
+    Props(
+      new FastSyncBranchResolver(
+        fastSync,
+        peerEventBus,
+        etcPeerManager,
+        blockchain,
+        syncConfig,
+        appStateStorage,
+        scheduler
+      )
+    )
 
   private val BlockHeadersHandlerName = "block-headers-request-handler"
 
   case class SearchState(minBlockNumber: BigInt, maxBlockNumber: BigInt)
 
   sealed trait BranchResolverRequest
-  case class StartBranchResolver() extends BranchResolverRequest
+  object StartBranchResolver extends BranchResolverRequest
 
   sealed trait BranchResolverResponse
-  case class BranchResolvedSuccessful() extends BranchResolverResponse
+  object BranchResolvedSuccessful extends BranchResolverResponse
 }
---
+++ src/test/scala/io/iohk/ethereum/blockchain/sync/fast/FastSyncBranchResolverSpec.scala
@@ -0,0 +1,166 @@
+package io.iohk.ethereum.blockchain.sync.fast
+
+import java.net.InetSocketAddress
+
+import akka.actor.{ActorRef, ActorSystem}
+import akka.pattern.gracefulStop
+import akka.testkit.TestActor.AutoPilot
+import akka.testkit.{TestKit, TestProbe}
+import akka.util.{ByteString, Timeout}
+import cats.effect.concurrent.Deferred
+import cats.implicits._
+import io.iohk.ethereum.blockchain.sync.PeerListSupport.PeersMap
+import io.iohk.ethereum.blockchain.sync._
+import io.iohk.ethereum.blockchain.sync.fast.FastSyncBranchResolver.{BranchResolvedSuccessful, StartBranchResolver}
+import io.iohk.ethereum.domain.{Block, ChainWeight}
+import io.iohk.ethereum.network.EtcPeerManagerActor._
+import io.iohk.ethereum.network.PeerEventBusActor.PeerEvent.MessageFromPeer
+import io.iohk.ethereum.network.p2p.messages.PV62.{BlockHeaders, GetBlockHeaders}
+import io.iohk.ethereum.network.p2p.messages.ProtocolVersions
+import io.iohk.ethereum.network.{EtcPeerManagerActor, Peer, PeerId}
+import io.iohk.ethereum.{BlockHelpers, NormalPatience, WithActorSystemShutDown}
+import monix.eval.Task
+import monix.execution.Scheduler
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.freespec.AnyFreeSpecLike
+
+import scala.concurrent.duration.DurationInt
+import scala.util.Random
+
+class FastSyncBranchResolverSpec
+    extends TestKit(ActorSystem("FastSyncBranchResolver_testing"))
+    with AnyFreeSpecLike
+    with ScalaFutures
+    with NormalPatience
+    with WithActorSystemShutDown { self =>
+  implicit val timeout: Timeout = Timeout(30.seconds)
+
+  import FastSyncBranchResolverSpec._
+
+  "FastSyncBranchResolver" - {
+    "fetch headers from the new master peer" - {
+      "resolving the chain from the first request to the new master peer, and then removes the last two blocks" in new TestSetup {
+        val sender = TestProbe("sender")(system)
+
+        val commonBlocks: List[Block] = BlockHelpers.generateChain(
+          5,
+          BlockHelpers.genesis,
+          block => block
+        )
+        val blocksSaved: List[Block] = commonBlocks :++ BlockHelpers.generateChain(
+          2,
+          commonBlocks.last,
+          block => block
+        )
+
+        val blocksSavedInPeer: List[Block] = commonBlocks :++ BlockHelpers.generateChain(
+          3,
+          commonBlocks.last,
+          block => block
+        )
+
+        saveBlocks(blocksSaved)
+        val etcPeerManager = createEtcPeerManager(handshakedPeers, blocksSavedInPeer)(scheduler)
+        val fastSyncBranchResolver = creatFastSyncBranchResolver(sender.ref, etcPeerManager)
+        sender.send(fastSyncBranchResolver, StartBranchResolver)
+
+        sender.expectMsg(BranchResolvedSuccessful)
+        assert(blockchain.getBestBlockNumber() === 5)
+        stopController(fastSyncBranchResolver)
+      }
+
+      //TODO: work in progress
+      /*"resolving the chain doing binary searching with the new master peer" in new TestSetup {
+
+      }*/
+    }
+  }
+
+  trait TestSetup extends EphemBlockchainTestSetup with TestSyncConfig with TestSyncPeers {
+    override implicit lazy val system = self.system
+    override implicit val scheduler = Scheduler(system.dispatcher)
+
+    def peerId(number: Int): PeerId = PeerId(s"peer_$number")
+    def getPeer(id: PeerId): Peer =
+      Peer(new InetSocketAddress("127.0.0.1", 0), TestProbe(id.value).ref, incomingConnection = false)
+    def getPeerInfo(peer: Peer, protocolVersion: Int = ProtocolVersions.PV64): PeerInfo = {
+      val status =
+        RemoteStatus(
+          protocolVersion,
+          1,
+          ChainWeight.totalDifficultyOnly(1),
+          ByteString(s"${peer.id}_bestHash"),
+          ByteString("unused")
+        )
+      PeerInfo(
+        status,
+        forkAccepted = true,
+        chainWeight = status.chainWeight,
+        maxBlockNumber = Random.between(1, 10),
+        bestBlockHash = status.bestHash
+      )
+    }
+
+    val handshakedPeers: PeersMap = (0 to 5).toList.map((peerId _).andThen(getPeer)).fproduct(getPeerInfo(_)).toMap
+
+    def saveBlocks(blocks: List[Block]): Unit = {
+      blocks.foreach(block => blockchain.save(block, Nil, ChainWeight.totalDifficultyOnly(1), saveAsBestBlock = true))
+    }
+
+    def createEtcPeerManager(peers: PeersMap, blocks: List[Block])(implicit scheduler: Scheduler): ActorRef = {
+      val etcPeerManager = TestProbe("etc_peer_manager")
+      val autoPilot =
+        new EtcPeerManagerAutoPilot(
+          peersConnectedDeferred,
+          peers,
+          blocks
+        )
+      etcPeerManager.setAutoPilot(autoPilot)
+      etcPeerManager.ref
+    }
+
+    def creatFastSyncBranchResolver(fastSync: ActorRef, etcPeerManager: ActorRef): ActorRef = system.actorOf(
+      FastSyncBranchResolver.prop(
+        fastSync = fastSync,
+        peerEventBus = TestProbe("peer_event_bus").ref,
+        etcPeerManager = etcPeerManager,
+        blockchain = blockchain,
+        syncConfig = syncConfig,
+        appStateStorage = storagesInstance.storages.appStateStorage,
+        scheduler = system.scheduler
+      )
+    )
+
+    def stopController(actorRef: ActorRef): Unit = {
+      awaitCond(gracefulStop(actorRef, actorAskTimeout.duration).futureValue)
+    }
+  }
+}
+
+object FastSyncBranchResolverSpec {
+
+  private val peersConnectedDeferred = Deferred.unsafe[Task, Unit]
+
+  class EtcPeerManagerAutoPilot(
+      peersConnected: Deferred[Task, Unit],
+      peers: PeersMap,
+      blocks: List[Block]
+  )(implicit scheduler: Scheduler)
+      extends AutoPilot {
+    def run(sender: ActorRef, msg: Any): EtcPeerManagerAutoPilot = {
+      msg match {
+        case EtcPeerManagerActor.GetHandshakedPeers =>
+          sender ! EtcPeerManagerActor.HandshakedPeers(peers)
+          peersConnected.complete(()).onErrorHandle(_ => ()).runSyncUnsafe()
+        case sendMsg @ EtcPeerManagerActor.SendMessage(rawMsg, peerId) =>
+          val response = rawMsg.underlyingMsg match {
+            case GetBlockHeaders(startingBlock, maxHeaders, _, false) =>
+              BlockHeaders(blocks.map(_.header))
+          }
+          val theResponse = MessageFromPeer(response, peerId)
+          sender ! theResponse
+      }
+      this
+    }
+  }
+}
