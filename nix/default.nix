input-output-hk/mantis

commit 5f38623048033d997d895b3bbc4efa4ca1340917

[ETCM-129] Scala 2.13 (#875)
* [ETCM-129] - Migrate to 2.13

Co-authored-by: dmitry-worker <dzenmassta@gmail.com>
Co-authored-by: Jonathan Ringer <jonathan.ringer@iohk.io>
Guided by: Piotr Paradzinski <piotr.paradzinski@iohk.io>
Reviewed by: Enrique Rodríguez <enrique.rodriguez@iohk.io>
Dependencies upgrade guide: Akosh Farkash <akosh.farkash@iohk.io>

nix/default.nix
@@ -1,25 +1,26 @@
 { system ? builtins.currentSystem, sources ? import ./sources.nix, src ? ../. }:
 
 let
-  overlay = final: prev: {
-    inherit sources;
+  # we need to filter out the nix files in this repository as they will
+  # affect the fixed derivation calculation from sbt-derivation
+  overlay = final: prev: let
     inherit (import sources.gitignore { inherit (prev) lib; }) gitignoreSource;
+    cleanedSrc = prev.lib.cleanSource (gitignoreSource src);
+  in {
+    inherit sources;
 
-    sbtix = prev.callPackage ../sbtix.nix {
-      jdk = prev.openjdk8_headless;
-      jre = prev.openjdk8_headless.jre;
+    # match java version used by devs, this should also change the version used by sbt
+    jre = prev.jdk8.jre;
+
+    mantis = final.callPackage ./pkgs/mantis.nix {
+      src = cleanedSrc;
     };
-
-    mantisPkgs = final.callPackage ./pkgs/mantis {
-      inherit (prev.openjdk8_headless) jre;
-      inherit src;
-    };
-
-    inherit (final.mantisPkgs) mantis;
-
-    mkSrc = import sources.nix-mksrc { inherit (final) lib; };
   };
+  sbt-derivation-overlay = import sources.sbt-derivation;
 in import sources.nixpkgs {
   inherit system;
-  overlays = [ overlay ];
+  overlays = [
+    sbt-derivation-overlay
+    overlay
+  ];
 }

nix/pkgs/default.nix
@@ -1,9 +1,7 @@
-{ pkgs, lib, newScope, sources, src, impure ? false }:
+{ pkgs, lib, src, impure ? false }:
 
-lib.makeScope newScope (self:
-  with self;
-  let callPackages = lib.callPackagesWith (pkgs // self);
-  in {
-    inherit sources src impure;
-    jdk = prev.openjdk8_headless;
-  })
+lib.makeScope pkgs.newScope (self: with self; {
+  inherit src impure;
+  mantis = callPackage ./mantis.nix { };
+  jdk = openjdk8_headless;
+})

nix/pkgs/mantis/default.nix
@@ -1,11 +1,0 @@
-{ src, mkSrc, lib, gitignoreSource, callPackage, jre }: rec {
-
-  mantis-source = mkSrc src;
-
-  mantis-unwrapped = callPackage ./unwrapped.nix { src = mantis-source; };
-
-  mantis = callPackage ./wrapped.nix {
-    mantis = mantis-unwrapped;
-    inherit jre;
-  };
-}

commit 2c4be1634c0052bd5b7f1e03461731d9fa534971

[FIX] Workaround for Nix's pure evaluation mode
This allows pure evaluation to work again, which was broken in
5f38623048033d997d895b3bbc4efa4ca1340917. This is needed for deployment with
mantis-ops, which uses Nix flakes, which enforce pure evaluation mode.

This command now works:

  nix-instantiate --pure-eval -E --argstr rev "$(git rev-parse @)" \
    '{ rev }: let src = fetchGit { url = ./.; inherit rev; }; in
     import src { inherit src; system = "x86_64-linux"; }'

nix/default.nix
@@ -5,7 +5,11 @@ let
   # affect the fixed derivation calculation from sbt-derivation
   overlay = final: prev: let
     inherit (import sources.gitignore { inherit (prev) lib; }) gitignoreSource;
-    cleanedSrc = prev.lib.cleanSource (gitignoreSource src);
+    # If src isn't a path, it's likely a prefiltered store path, so use it directly
+    # This also makes Nix flakes work by passing the flake source as the `src` arg
+    cleanedSrc = if builtins.isPath src
+      then prev.lib.cleanSource (gitignoreSource src)
+      else src;
   in {
     inherit sources;
 