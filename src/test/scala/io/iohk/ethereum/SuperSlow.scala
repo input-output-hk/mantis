c63592da682ec213f03ff62a79fc82d9d6068ff8
Jaap van der Plas <jaap.van.der.plas@iohk.io>
2021-02-03 20:42:38 +0100

    ETCM-607 skip most mining-related assertions in EthashUtilsSpec on CI for faster builds

---
+++ src/test/scala/io/iohk/ethereum/SuperSlow.scala
@@ -0,0 +1,15 @@
+package io.iohk.ethereum
+
+import com.typesafe.config.ConfigFactory
+
+trait SuperSlow {
+  lazy private val skip = ConfigFactory.load().getBoolean("skip-super-slow-tests")
+
+  /**
+    * Some assertions may be prohibitively slow and shouldn't run on every CI run. Use this method when that's the case.
+    *
+    * @param f slow tests
+    */
+  def superSlow(f: => Any): Unit =
+    if (!skip) f
+}
