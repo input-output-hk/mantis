8e377fb9bcc052051ea872a1d5ea14d96e83bb0e
Michael Fellinger <michael.fellinger@iohk.io>
2021-03-08 13:16:29 +0100

    add nomad entrypoint

---
+++ nix/entrypoint.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+
+
+mkdir -p /tmp
+mkdir -p "$NOMAD_TASK_DIR/mantis"
+cd "$NOMAD_TASK_DIR"
+name="java"
+
+if [ -n "${DAG_NAME:-}" ]; then
+  if [ -f "ethash/$DAG_NAME" ]; then
+    echo "found existing DAG"
+    sha256sum "ethash/$DAG_NAME"
+  else
+    mkdir -p ethash
+    aws \
+      --endpoint-url "$MONITORING_ADDR" \
+      s3 cp \
+      "s3://$DAG_BUCKET/$DAG_NAME" \
+      "ethash/$DAG_NAME" \
+      || echo "Unable to download DAG, skipping."
+  fi
+fi
+
+set +x
+until [ "$(grep -c enode mantis.conf)" -ge "$REQUIRED_PEER_COUNT" ]; do
+  sleep 1
+done
+
+if [ -n "${DAG_NAME:-}" ]; then
+  delay="$((REQUIRED_PEER_COUNT * 100))"
+  echo "waiting for $delay seconds before start"
+  sleep "$delay"
+fi
+set -x
+
+ulimit -c unlimited
+cp mantis.conf running.conf
+
+(
+while true; do
+  set +x
+  while diff -u running.conf mantis.conf > /dev/stderr; do
+    sleep 900
+  done
+  set -x
+
+  if ! diff -u running.conf mantis.conf > /dev/stderr; then
+    echo "Found updated config file, restarting Mantis"
+    cp mantis.conf running.conf
+    cat running.conf > /dev/stderr
+    pkill "$name" || true
+  fi
+done
+) &
+
+starts=0
+while true; do
+  starts="$((starts+1))"
+  echo "Start Number $starts" > /dev/stderr
+  mantis "-Duser.home=$NOMAD_TASK_DIR" "$@" || true
+  sleep 10
+done
--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,11 +1,27 @@
 rev: final: prev: {
   jre = prev.jdk8.jre;
+
   mantis = final.callPackage ./mantis.nix {
     src = builtins.fetchGit {
-      url = https://github.com/input-output-hk/mantis;
+      url = "https://github.com/input-output-hk/mantis";
       ref = "flake";
       rev = rev;
       submodules = true;
     };
   };
+
+  writeBashChecked = final.writers.makeScriptWriter {
+    interpreter = "${final.bashInteractive}/bin/bash";
+    check = final.writers.writeBash "shellcheck-check" ''
+      ${final.shellcheck}/bin/shellcheck -x "$1"
+    '';
+  };
+
+  writeBashBinChecked = name: final.writeBashChecked "/bin/${name}";
+
+  mantis-entrypoint = final.writeBashBinChecked "mantis-entrypoint" ''
+    export PATH=${final.lib.makeBinPath [ final.coreutils final.restic ]}
+
+    ${builtins.readFile ./entrypoint.sh}
+  '';
 }
