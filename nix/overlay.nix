1906f54e2e8524dd957cc64c9f6b75e09d57f3f0
Michael Fellinger <michael.fellinger@iohk.io>
2021-03-10 22:29:20 +0100

    entrypoint needs procps

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -22,7 +22,7 @@ rev: final: prev: {
   mantis-entrypoint-script = final.writeBashBinChecked "mantis-entrypoint" ''
     export PATH=${
       final.lib.makeBinPath
-      (with final; [ coreutils restic gnugrep awscli diffutils mantis ])
+      (with final; [ coreutils restic gnugrep awscli diffutils mantis procps ])
     }
 
     ${builtins.readFile ./entrypoint.sh}
