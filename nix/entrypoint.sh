ff021e914f8c772a6db1da52de4f724509d4d2d2
Michael Fellinger <michael.fellinger@iohk.io>
2021-03-12 13:43:24 +0100

    shorten the time between config updates when mantis is unstable

--- nix/entrypoint.sh
+++ nix/entrypoint.sh
@@ -66,8 +66,6 @@ while true; do
 
   if ! diff -u running.conf mantis.conf > /dev/stderr; then
     echo "Found updated config file, restarting Mantis"
-    cp mantis.conf running.conf
-    cat running.conf > /dev/stderr
     pkill "$name" || true
   fi
 done
@@ -77,6 +75,8 @@ starts=0
 while true; do
   starts="$((starts+1))"
   echo "Start Number $starts" > /dev/stderr
+  cp mantis.conf running.conf
+  cat running.conf > /dev/stderr
   rm -f "$NOMAD_TASK_DIR/mantis/rocksdb/LOCK"
   mantis "-Duser.home=$NOMAD_TASK_DIR" "$@" || true
   sleep 10
