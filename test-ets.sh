dd645e06ac65e172b86de2b78fcb24476f3d74e9
Jaap van der Plas <jaap.van.der.plas@iohk.io>
2021-04-19 16:56:29 +0200

    create annotation with retesteth results

--- test-ets.sh
+++ test-ets.sh
@@ -8,17 +8,27 @@ git submodule init;
 git submodule update;
 
 echo "booting Mantis and waiting for RPC API to be up"
-$SBT -Dconfig.file=./src/main/resources/conf/testmode.conf run > mantis-stdout.txt 2> mantis-stderr.txt &
+$SBT -Dconfig.file=./src/main/resources/conf/testmode.conf run &> mantis-log.txt &
 
 while ! nc -z localhost 8546; do   
   sleep 0.1
 done
 
-echo "running retesteth"
-retesteth -- --testpath src/ets/resources/ets --datadir src/ets/resources/config --clients mantis > retesteth-stdout.txt 2> retesteth-stderr.txt
+echo "running retesteth GeneralStateTests"
+retesteth -t GeneralStateTests -- --testpath src/ets/resources/ets --datadir src/ets/resources/config --clients mantis > retesteth-log.txt
 code=$?
+summary=$(sed -n '/Total Tests Run/,$p' retesteth-log.txt)
 
 echo "retesteth exited with code $code"
+
+cat <<EOF | buildkite-agent annotate --context "retesteth" --style "info"
+  <details>
+    <summary>retesteth: GeneralStateTests</summary>
+    ```term
+      $summary
+    ```
+  </details>
+EOF
 
 echo "shutting down mantis"
 kill %1
