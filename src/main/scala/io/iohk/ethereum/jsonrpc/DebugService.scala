48b39b57ed135da493997b705ac9430df16aab50
Jaap van der Plas <jaap.van.der.plas@iohk.io>
2021-04-26 16:05:51 +0200

    [ETCM-797] add debug endpoint to request a specific block (by number) from connected peers

--- src/main/scala/io/iohk/ethereum/jsonrpc/DebugService.scala
+++ src/main/scala/io/iohk/ethereum/jsonrpc/DebugService.scala
@@ -3,10 +3,10 @@ package io.iohk.ethereum.jsonrpc
 import akka.actor.ActorRef
 import akka.util.Timeout
 import io.iohk.ethereum.jsonrpc.AkkaTaskOps._
-import io.iohk.ethereum.jsonrpc.DebugService.{ListPeersInfoRequest, ListPeersInfoResponse}
 import io.iohk.ethereum.network.EtcPeerManagerActor.{PeerInfo, PeerInfoResponse}
 import io.iohk.ethereum.network.PeerManagerActor.Peers
 import io.iohk.ethereum.network.{EtcPeerManagerActor, Peer, PeerActor, PeerId, PeerManagerActor}
+import io.iohk.ethereum.network.p2p.messages.PV62.GetBlockHeaders
 import monix.eval.Task
 
 import scala.concurrent.duration._
@@ -14,9 +14,13 @@ import scala.concurrent.duration._
 object DebugService {
   case class ListPeersInfoRequest()
   case class ListPeersInfoResponse(peers: List[PeerInfo])
+
+  case class GetSpecificBlockRequest(number: BigInt)
+  case class GetSpecificBlockResponse()
 }
 
 class DebugService(peerManager: ActorRef, etcPeerManager: ActorRef) {
+  import DebugService._
 
   def listPeersInfo(getPeersInfoRequest: ListPeersInfoRequest): ServiceResponse[ListPeersInfoResponse] = {
     for {
@@ -41,4 +45,24 @@ class DebugService(peerManager: ActorRef, etcPeerManager: ActorRef) {
       .askFor[PeerInfoResponse](EtcPeerManagerActor.PeerInfoRequest(peer))
       .map(resp => resp.peerInfo)
   }
+
+  def getSpecificBlock(req: GetSpecificBlockRequest): ServiceResponse[GetSpecificBlockResponse] =
+    for {
+      ids <- getPeerIds
+      peers <- Task.traverse(ids)(id => getPeerInfo(id).map(id -> _))
+      eligiblePeers = peers.collect {
+        case (id, Some(peerInfo)) if peerInfo.maxBlockNumber >= req.number => id
+      }
+      _ <- Task.now {
+        eligiblePeers.foreach { peerId =>
+          peerManager.tell(
+            PeerManagerActor.SendMessage(
+              GetBlockHeaders(Left(req.number - 1), 2, 0, false),
+              peerId
+            ),
+            peerManager
+          )
+        }
+      }
+    } yield Right(GetSpecificBlockResponse())
 }
