0605e8e27d62156050895351be611861d99646bf
Lukasz Golebiewski <lukasz.golebiewski@iohk.io>
2021-07-07 15:37:42 +0200

    [ETCM-355] Log negotiated protocol version

--- src/main/scala/io/iohk/ethereum/network/handshaker/EtcHelloExchangeState.scala
+++ src/main/scala/io/iohk/ethereum/network/handshaker/EtcHelloExchangeState.scala
@@ -31,9 +31,18 @@ case class EtcHelloExchangeState(handshakerConfiguration: EtcHandshakerConfigura
     log.debug("Protocol handshake finished with peer ({})", hello)
     // FIXME in principle this should be already negotiated
     Capability.negotiate(hello.capabilities.toList, handshakerConfiguration.blockchainConfig.capabilities) match {
-      case Some(ProtocolVersions.ETC64) => EtcNodeStatus64ExchangeState(handshakerConfiguration)
-      case Some(ProtocolVersions.ETH63) => EthNodeStatus63ExchangeState(handshakerConfiguration)
-      case Some(ProtocolVersions.ETH64) => EthNodeStatus64ExchangeState(handshakerConfiguration)
+      case Some(ProtocolVersions.ETC64) => {
+        log.debug("Negotiated protocol version with client {} is etc/64", hello.clientId)
+        EtcNodeStatus64ExchangeState(handshakerConfiguration)
+      }
+      case Some(ProtocolVersions.ETH63) => {
+        log.debug("Negotiated protocol version with client {} is eth/63", hello.clientId)
+        EthNodeStatus63ExchangeState(handshakerConfiguration)
+      }
+      case Some(ProtocolVersions.ETH64) => {
+        log.debug("Negotiated protocol version with client {} is eth/64", hello.clientId)
+        EthNodeStatus64ExchangeState(handshakerConfiguration)
+      }
       case _ =>
         log.debug(
           s"Connected peer does not support {} / {} / {} protocol. Disconnecting.",
