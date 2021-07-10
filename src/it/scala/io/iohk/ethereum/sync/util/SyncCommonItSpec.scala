98e04d0e5b37684d6be2218ec51ea9d6780c1bee
Dominik Zajkowski <dominik.zajkowsk@iohk.io>
2021-07-10 18:46:34 +0200

    [Kaizen] Unflake RegularSyncSpec

--- src/it/scala/io/iohk/ethereum/sync/util/SyncCommonItSpec.scala
+++ src/it/scala/io/iohk/ethereum/sync/util/SyncCommonItSpec.scala
@@ -3,6 +3,8 @@ package io.iohk.ethereum.sync.util
 import java.net.InetSocketAddress
 import java.net.ServerSocket
 
+import scala.util.Using
+
 import io.iohk.ethereum.domain.Block
 import io.iohk.ethereum.domain.ChainWeight
 import io.iohk.ethereum.ledger.InMemoryWorldStateProxy
@@ -10,11 +12,11 @@ import io.iohk.ethereum.ledger.InMemoryWorldStateProxy
 object SyncCommonItSpec {
   val IdentityUpdate: (BigInt, InMemoryWorldStateProxy) => InMemoryWorldStateProxy = (_, world) => world
 
-  def randomAddress(): InetSocketAddress = {
-    val s = new ServerSocket(0)
-    try new InetSocketAddress("localhost", s.getLocalPort)
-    finally s.close()
-  }
+  def randomAddress(): InetSocketAddress =
+    Using(new ServerSocket(0)) { s =>
+      s.setReuseAddress(true) // read the javadoc and don't remove this line
+      try new InetSocketAddress("localhost", s.getLocalPort)
+    }.get
 
   final case class BlockchainState(
       bestBlock: Block,
