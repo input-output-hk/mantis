23a039d24949fcfc26e46a1482fd4c09883ad286
Leonor Boga <leonor.boga@iohk.io>
2021-08-18 16:49:51 +0200

    [ETCM-1082] Add BlockMetadataStorage and BlockMetadataStorageSpec

---
+++ src/main/scala/io/iohk/ethereum/db/storage/BlockMetadataStorage.scala
@@ -0,0 +1,23 @@
+package io.iohk.ethereum.db.storage
+
+import akka.util.ByteString
+import boopickle.Default._
+import io.iohk.ethereum.db.dataSource.DataSource
+import io.iohk.ethereum.db.storage.StorageTypes.BlockHash
+import io.iohk.ethereum.utils.ByteUtils.{byteSequenceToBuffer, compactPickledBytes}
+
+case class BlockMetadata(isExecuted: Boolean)
+
+class BlockMetadataStorage(val dataSource: DataSource) extends TransactionalKeyValueStorage[BlockHash, BlockMetadata] {
+  override val namespace: IndexedSeq[Byte] = Namespaces.HeightsNamespace
+
+  override def keySerializer: BlockHash => IndexedSeq[Byte] = identity
+
+  override def keyDeserializer: IndexedSeq[Byte] => BlockHash = bytes => ByteString(bytes.toArray[Byte])
+
+  override def valueSerializer: BlockMetadata => IndexedSeq[Byte] =
+    (Pickle.intoBytes[BlockMetadata] _).andThen(compactPickledBytes)
+
+  override def valueDeserializer: IndexedSeq[Byte] => BlockMetadata =
+    (byteSequenceToBuffer _).andThen(Unpickle[BlockMetadata].fromBytes)
+}
