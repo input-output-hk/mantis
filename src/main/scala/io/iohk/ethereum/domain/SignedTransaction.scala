fb557e6b24c83188bae10627fc48bf2d935bbd15
Michaël Strauss <85114085+strauss-m@users.noreply.github.com>
2021-08-25 11:40:13 +0200

    [ETCM-1096] Add transaction type 1 signature payload

--- src/main/scala/io/iohk/ethereum/domain/SignedTransaction.scala
+++ src/main/scala/io/iohk/ethereum/domain/SignedTransaction.scala
@@ -68,8 +68,8 @@ object SignedTransaction {
       chainId: Option[Byte]
   ): SignedTransaction = {
     val bytes = bytesToSign(tx, chainId)
-    val sig = ECDSASignature.sign(bytes, keyPair /*, chainId*/ )
-    SignedTransaction(tx, getEthereumSignature(sig, chainId))
+    val sig = ECDSASignature.sign(bytes, keyPair)
+    SignedTransaction(tx, getEthereumSignature(tx, sig, chainId))
   }
 
   private def bytesToSign(tx: Transaction, chainId: Option[Byte]): Array[Byte] =
@@ -80,7 +80,10 @@ object SignedTransaction {
         generalTransactionBytes(tx)
     }
 
-  /** Convert a RLP compatible ECDSA Signature to a raw crypto signature.
+  /** Transaction specific piece of code.
+    * This should be moved to the Signer architecture once available.
+    *
+    * Convert a RLP compatible ECDSA Signature to a raw crypto signature.
     * Depending on the transaction type and the block number, different rules are
     * used to enhance the v field with additional context for signing purpose and networking
     * communication.
@@ -88,11 +91,32 @@ object SignedTransaction {
     * Currently, both semantic data are represented by the same data structure.
     *
     * @see getEthereumSignature for the reciprocal conversion.
+    * @param signedTransaction the signed transaction from which to extract the raw signature
+    * @return a raw crypto signature, with only 27 or 28 as valid ECDSASignature.v value
+    */
+  private def getRawSignature(signedTransaction: SignedTransaction): ECDSASignature =
+    signedTransaction.tx match {
+      case _: LegacyTransaction =>
+        val chainIdOpt = extractChainId(signedTransaction)
+        getLegacyTransactionRawSignature(signedTransaction.signature, chainIdOpt)
+      case _: TransactionWithAccessList =>
+        getTWALRawSignature(signedTransaction.signature)
+      case _ => throw new IllegalArgumentException(s"Transaction type not supported for $signedTransaction")
+    }
+
+  /** Transaction specific piece of code.
+    * This should be moved to the Signer architecture once available.
+    *
+    * Convert a LegacyTransaction RLP compatible ECDSA Signature to a raw crypto signature
+    *
     * @param ethereumSignature the v-modified signature, received from the network
     * @param chainIdOpt        the chainId if available
     * @return a raw crypto signature, with only 27 or 28 as valid ECDSASignature.v value
     */
-  private def getRawSignature(ethereumSignature: ECDSASignature, chainIdOpt: Option[Byte]): ECDSASignature =
+  private def getLegacyTransactionRawSignature(
+      ethereumSignature: ECDSASignature,
+      chainIdOpt: Option[Byte]
+  ): ECDSASignature =
     chainIdOpt match {
       // ignore chainId for unprotected negative y-parity in pre-eip155 signature
       case Some(_) if ethereumSignature.v == ECDSASignature.negativePointSign =>
@@ -111,11 +135,33 @@ object SignedTransaction {
       // unexpected chainId
       case _ =>
         throw new IllegalStateException(
-          s"Unexpected pointSign. ChainId: ${chainIdOpt.getOrElse("None")}, ethereum.signature.v: ${ethereumSignature.v}"
+          s"Unexpected pointSign for LegacyTransaction, chainId: ${chainIdOpt
+            .getOrElse("None")}, ethereum.signature.v: ${ethereumSignature.v}"
         )
     }
 
-  /** Convert a RLP compatible ECDSA Signature to a raw crypto signature.
+  /** Transaction specific piece of code.
+    * This should be moved to the Signer architecture once available.
+    *
+    * Convert a TransactionWithAccessList RLP compatible ECDSA Signature to a raw crypto signature
+    *
+    * @param ethereumSignature the v-modified signature, received from the network
+    * @return a raw crypto signature, with only 27 or 28 as valid ECDSASignature.v value
+    */
+  private def getTWALRawSignature(ethereumSignature: ECDSASignature): ECDSASignature =
+    ethereumSignature.v match {
+      case 0 => ethereumSignature.copy(v = ECDSASignature.negativePointSign)
+      case 1 => ethereumSignature.copy(v = ECDSASignature.positivePointSign)
+      case _ =>
+        throw new IllegalStateException(
+          s"Unexpected pointSign for TransactionWithAccessList, ethereum.signature.v: ${ethereumSignature.v}"
+        )
+    }
+
+  /** Transaction specific piece of code.
+    * This should be moved to the Signer architecture once available.
+    *
+    * Convert a raw crypto signature into a RLP compatible ECDSA one.
     * Depending on the transaction type and the block number, different rules are
     * used to enhance the v field with additional context for signing purpose and networking
     * communication.
@@ -123,11 +169,30 @@ object SignedTransaction {
     * Currently, both semantic data are represented by the same data structure.
     *
     * @see getRawSignature for the reciprocal conversion.
-    * @param ethereumSignature the v-modified signature, received from the network
-    * @param chainIdOpt        the chainId if available
-    * @return a raw crypto signature, with only 27 or 28 as valid ECDSASignature.v value
+    * @param tx           the transaction to adapt the raw signature to
+    * @param rawSignature the raw signature generated by the crypto module
+    * @param chainIdOpt   the chainId if available
+    * @return a ECDSASignature with v value depending on the transaction type
     */
-  private def getEthereumSignature(rawSignature: ECDSASignature, chainIdOpt: Option[Byte]): ECDSASignature =
+  private def getEthereumSignature(tx: Transaction, rawSignature: ECDSASignature, chainIdOpt: Option[Byte]): ECDSASignature =
+    tx match {
+      case _: LegacyTransaction =>
+        getLegacyEthereumSignature(rawSignature, chainIdOpt)
+      case _: TransactionWithAccessList =>
+        getTWALEthereumSignature(rawSignature)
+      case _ => throw new IllegalArgumentException(s"Transaction type not supported for $tx")
+    }
+
+  /** Transaction specific piece of code.
+    * This should be moved to the Signer architecture once available.
+    *
+    * Convert a raw crypto signature into a RLP compatible ECDSA one.
+    *
+    * @param rawSignature the raw signature generated by the crypto module
+    * @param chainIdOpt        the chainId if available
+    * @return a legacy transaction specific ECDSASignature, with v chainId-protected if possible
+    */
+  private def getLegacyEthereumSignature(rawSignature: ECDSASignature, chainIdOpt: Option[Byte]): ECDSASignature =
     chainIdOpt match {
       case Some(chainId) if rawSignature.v == ECDSASignature.negativePointSign =>
         rawSignature.copy(v = (chainId * 2 + EIP155NegativePointSign).toByte)
@@ -141,19 +206,31 @@ object SignedTransaction {
         )
     }
 
+  /** Transaction specific piece of code.
+    * This should be moved to the Signer architecture once available.
+    *
+    * Convert a raw crypto signature into a RLP compatible ECDSA one.
+    *
+    * @param rawSignature the raw signature generated by the crypto module
+    * @return a transaction-with-access-list specific ECDSASignature
+    */
+  private def getTWALEthereumSignature(rawSignature: ECDSASignature): ECDSASignature =
+    rawSignature match {
+      case ECDSASignature(_, _, ECDSASignature.positivePointSign) => rawSignature.copy(v = ECDSASignature.positiveYParity)
+      case ECDSASignature(_, _, ECDSASignature.negativePointSign) => rawSignature.copy(v = ECDSASignature.negativeYParity)
+      case _ =>
+        throw new IllegalStateException(
+          s"Unexpected pointSign. raw.signature.v: ${rawSignature.v}, authorized values are ${ECDSASignature.allowedPointSigns
+            .mkString(", ")}"
+        )
+    }
+
   def getSender(tx: SignedTransaction): Option[Address] =
     Option(txSenders.getIfPresent(tx.hash)).orElse(calculateSender(tx))
 
   private def calculateSender(tx: SignedTransaction): Option[Address] = Try {
-    val ECDSASignature(_, _, v) = tx.signature
-    // chainId specific code that will be refactored with the Signer feature (ETCM-1096)
-    val chainIdOpt = extractChainId(tx)
-    val bytesToSign: Array[Byte] = chainIdOpt match {
-      case None          => generalTransactionBytes(tx.tx)
-      case Some(chainId) => chainSpecificTransactionBytes(tx.tx, chainId)
-    }
-
-    val recoveredPublicKey: Option[Array[Byte]] = getRawSignature(tx.signature, chainIdOpt).publicKey(bytesToSign)
+    val bytesToSign: Array[Byte] = getBytesToSign(tx)
+    val recoveredPublicKey: Option[Array[Byte]] = getRawSignature(tx).publicKey(bytesToSign)
 
     for {
       key <- recoveredPublicKey
@@ -179,11 +256,28 @@ object SignedTransaction {
   private def calculateAndCacheSender(stx: SignedTransaction) =
     calculateSender(stx).foreach(address => txSenders.put(stx.hash, address))
 
+  /** Transaction specific piece of code.
+    * This should be moved to the Signer architecture once available.
+    *
+    * Extract pre-eip 155 payload to sign for legacy transaction
+    *
+    * @param tx
+    * @return the transaction payload for Legacy transaction
+    */
   private def generalTransactionBytes(tx: Transaction): Array[Byte] = {
     val receivingAddressAsArray: Array[Byte] = tx.receivingAddress.map(_.toArray).getOrElse(Array.emptyByteArray)
     crypto.kec256(rlpEncode(RLPList(tx.nonce, tx.gasPrice, tx.gasLimit, receivingAddressAsArray, tx.value, tx.payload)))
   }
 
+  /** Transaction specific piece of code.
+    * This should be moved to the Signer architecture once available.
+    *
+    * Extract post-eip 155 payload to sign for legacy transaction
+    *
+    * @param tx
+    * @param chainId
+    * @return the transaction payload for Legacy transaction
+    */
   private def chainSpecificTransactionBytes(tx: Transaction, chainId: Byte): Array[Byte] = {
     val receivingAddressAsArray: Array[Byte] = tx.receivingAddress.map(_.toArray).getOrElse(Array.emptyByteArray)
     crypto.kec256(
@@ -203,6 +297,12 @@ object SignedTransaction {
     )
   }
 
+  /** Transaction specific piece of code.
+    * This should be moved to the Signer architecture once available.
+    *
+    * @param stx the signed transaction to get the chainId from
+    * @return Some(chainId) if available, None if not (unprotected signed transaction)
+    */
   private def extractChainId(stx: SignedTransaction): Option[Byte] = {
     val chainIdOpt: Option[BigInt] = stx.tx match {
       case _: LegacyTransaction
@@ -212,6 +312,62 @@ object SignedTransaction {
       case twal: TransactionWithAccessList => Some(twal.chainId)
     }
     chainIdOpt.map(_.toByte)
+  }
+
+  /** Transaction specific piece of code.
+    * This should be moved to the Signer architecture once available.
+    *
+    * @param transaction the signed transaction from which to extract the payload to sign
+    * @return the payload to sign
+    */
+  private def getBytesToSign(signedTransaction: SignedTransaction): Array[Byte] =
+    signedTransaction.tx match {
+      case _: LegacyTransaction            => getLegacyBytesToSign(signedTransaction)
+      case twal: TransactionWithAccessList => getTWALBytesToSign(twal)
+      case _                               => throw new IllegalArgumentException(s"unknown transaction type for $signedTransaction")
+    }
+
+  /** Transaction specific piece of code.
+    * This should be moved to the Signer architecture once available.
+    *
+    * Extract pre-eip / post-eip 155 payload to sign for legacy transaction
+    *
+    * @param signedTransaction
+    * @return the transaction payload for Legacy transaction
+    */
+  private def getLegacyBytesToSign(signedTransaction: SignedTransaction): Array[Byte] = {
+    val chainIdOpt = extractChainId(signedTransaction)
+    chainIdOpt match {
+      case None          => generalTransactionBytes(signedTransaction.tx)
+      case Some(chainId) => chainSpecificTransactionBytes(signedTransaction.tx, chainId)
+    }
+  }
+
+  /** Transaction specific piece of code.
+    * This should be moved to the Signer architecture once available.
+    *
+    * Extract payload to sign for Transaction with access list
+    *
+    * @param tx
+    * @return the transaction payload to sign for Transaction with access list
+    */
+  private def getTWALBytesToSign(tx: TransactionWithAccessList): Array[Byte] = {
+    import io.iohk.ethereum.network.p2p.messages.BaseETH6XMessages.accessListItemCodec
+    val receivingAddressAsArray: Array[Byte] = tx.receivingAddress.map(_.toArray).getOrElse(Array.emptyByteArray)
+    crypto.kec256(
+      rlpEncode(
+        RLPList(
+          tx.chainId,
+          tx.nonce,
+          tx.gasPrice,
+          tx.gasLimit,
+          receivingAddressAsArray,
+          tx.value,
+          tx.payload,
+          fromRlpList[AccessListItem](tx.accessList).toList
+        )
+      )
+    )
   }
 
   val byteArraySerializable: ByteArraySerializable[SignedTransaction] = new ByteArraySerializable[SignedTransaction] {
