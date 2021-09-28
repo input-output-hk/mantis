f6027d793637fe100a812cd464968f2e57d20faa
Jaap van der Plas <jaap.van.der.plas@iohk.io>
2021-09-28 20:26:17 +0200

    revert solc version

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url =
-    "github:nixos/nixpkgs?rev=a98302aa9b9628915878a6ea9776c40a0bb02950";
+    "github:nixos/nixpkgs?rev=98747f27ecfee70c8c97b195cbb94df80a074dda";
   inputs.sbt-derivation.url = "github:zaninime/sbt-derivation";
 
   outputs = { self, nixpkgs, flake-utils, sbt-derivation
@@ -49,12 +49,13 @@
       defaultApp = apps.mantis;
     }) // (collectHydraSets
       (map (name: mkHydraSet [ name ] [ "x86_64-linux" ]) [
+        "jdk17"
         "jdk8"
-        "lllc"
+        # "lllc"
         "mantis"
-        "mantis-entrypoint"
+        # "mantis-entrypoint"
         "netcat-gnu"
-        "retesteth"
+        # "retesteth"
         "sbt"
         "solc"
       ]));
